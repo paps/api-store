f29d690063ed324d3fba1167c9d2f2aff892d56c
Martin Tapia <contact@martintapia.com>
2018-02-22 18:48:14 +0100

    LI accept invites: fixes, lib-LinkedIn

--- store/LinkedIn Accept Invitations.js
+++ store/LinkedIn Accept Invitations.js
@@ -1,7 +1,7 @@
 // Phantombuster configuration {
 "phantombuster command: nodejs"
 "phantombuster package: 4"
-"phantombuster dependencies: lib-StoreUtilities.js"
+"phantombuster dependencies: lib-StoreUtilities.js, lib-LinkedIn.js"
 
 const Buster = require("phantombuster")
 const buster = new Buster()
@@ -18,26 +18,9 @@ const nick = new Nick({
 
 const StoreUtilities = require("./lib-StoreUtilities")
 const utils = new StoreUtilities(nick, buster)
+const LinkedIn = require("./lib-LinkedIn")
+const linkedIn = new LinkedIn(nick, buster, utils)
 // }
-
-const linkedinConnect = async (tab, cookie, url) => {
-	if (typeof url === 'undefined' || url === null)
-		url = "https://www.linkedin.com"
-	await tab.setCookie({
-		"name": "li_at",
-		"value": cookie,
-		"domain": ".www.linkedin.com",
-	})
-	await tab.open(url)
-	try {
-		await tab.waitUntilVisible("#extended-nav", 10000)
-	} catch (err) {
-		const sc = `error1.png`
-		await tab.screenshot(sc)
-		await buster.save(sc)
-		throw "Can't connect to LinkedIn with this session cookie."
-	}
-}
 
 // Accept all profiles visible on the page and returns an Array of added profiles.
 const acceptInvites = async (tab, nbProfiles) => {
@@ -45,7 +28,7 @@ const acceptInvites = async (tab, nbProfiles) => {
 		jQuery.noConflict()
 		const invites = jQuery("ul.mn-invitation-list > li").map(function (i) {
 			if (i < arg.nbProfiles) {
-				jQuery(this).find("label.mn-person-card__checkbox-label").click()
+				jQuery(this).find("input[type='checkbox']").click()
 				return this.querySelector("a.mn-person-info__link").href
 			}
 		})
@@ -56,34 +39,34 @@ const acceptInvites = async (tab, nbProfiles) => {
 const loadProfilesUsingScrollDown = async (tab) => {
 	utils.log("Scrolling down...", "loading")
 	await tab.scrollToBottom()
-	await tab.wait(1000)
+	await tab.wait(3000)
 	await tab.scrollToBottom()
-	await tab.wait(1000)
+	await tab.wait(3000)
+	await tab.scrollToBottom()
+	await tab.wait(3000)
 }
 
 nick.newTab().then(async (tab) => {
-	const sessionCookie = buster.arguments.sessionCookie
-	const numberOfProfilesToAdd = buster.arguments.numberOfProfilesToAdd || 50
+	const {sessionCookie, numberOfProfilesToAdd} = utils.validateArguments()
 
-	if (typeof sessionCookie !== "string" || sessionCookie.length <= 0) {
-		throw "Warning: Your session cookie isn't valid."
-	}
-	utils.log("Connecting to linkedIn...", "loading")
-	await linkedinConnect(tab, sessionCookie, "https://www.linkedin.com/mynetwork/invitation-manager/?filterCriteria=null")
+	await linkedIn.login(tab, sessionCookie, "https://www.linkedin.com/mynetwork/invitation-manager/?filterCriteria=null")
 	await tab.inject("../injectables/jquery-3.0.0.min.js")
-	const selector = await tab.waitUntilVisible(["label.mn-person-card__checkbox-label", "section.mn-invitation-manager__no-invites"], 5000, "or")
+	const selector = await tab.waitUntilVisible(["label.mn-person-card__checkbox-label", "section.mn-invitation-manager__no-invites"], 10000, "or")
 	if (selector === "section.mn-invitation-manager__no-invites") {
-		utils.log("No invite to accept.", "warning")
+		utils.log("No invite to accept.", "done")
 		nick.exit()
 	}
 	await loadProfilesUsingScrollDown(tab)
 	let invites = await acceptInvites(tab, numberOfProfilesToAdd)
 	await tab.click(`button[data-control-name="accept_all"]`)
 
+	await tab.wait(2000)
+
 	// Verbose
-	utils.log(`A total of ${invites.length} profile${invites.length != 1 ? 's has' : ' have'} been added`, "done")
+	utils.log(`A total of ${invites.length} profile${invites.length != 1 ? 's have' : ' has'} been added`, "done")
 	for (invite of invites)
 		console.log(`\t${invite}`)
+	await linkedIn.saveCookie()
 })
 .then(() => {
 	utils.log("Job done!", "done")
