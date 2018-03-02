096c1dcd02a95abc2f6951d7333c1f09a6ba1d48
Martin Tapia <contact@martintapia.com>
2018-03-02 21:00:17 +0100

    utilities: exit 3 min before script end for 'long running' bots

--- libs/lib-StoreUtilities.js
+++ libs/lib-StoreUtilities.js
@@ -10,6 +10,7 @@ class StoreUtilities {
 	constructor(nick, buster) {
 		this.nick = nick
 		this.buster = buster
+		this.minTimeBeforeExit = null // will be decided on the first call to checkTimeLeft()
 		if (buster.arguments.testRunObject) {
 			this.test = true
 			this.testRunObject = buster.arguments.testRunObject
@@ -43,6 +44,7 @@ class StoreUtilities {
 		}
 	}
 
+	// New way of checking arguments. Uses provided schema.
 	validateArguments() {
 		if (this.buster.argumentSchema) {
 			const validate = validator(this.buster.argumentSchema)
@@ -57,7 +59,185 @@ class StoreUtilities {
 		return this.buster.arguments
 	}
 
+
+	// Function to get data from a google spreadsheet or from a csv
+	async getDataFromCsv(url, columnName, printLogs = true) {
+		const buster = this.buster
+		if (printLogs) {
+			this.log(`Getting data from ${url}...`, "loading")
+		}
+		const urlRegex = /^((http[s]?|ftp):\/)?\/?([^:\/\s]+)(:([^\/]*))?((\/[\w\/-]+)*\/)([\w\-\.]+[^#?\s]+)(\?([^#]*))?(#(.*))?$/
+		const match = url.match(urlRegex)
+		if (match) {
+			if (match[3] === "docs.google.com") {
+				if (match[8] === "edit") {
+					url = `https://docs.google.com/${match[6]}export?format=csv`
+				} else {
+					url = `https://docs.google.com/spreadsheets/d/${match[8].replace(/\/$/, "")}/export?format=csv`
+				}
+			}
+			await buster.download(url, "sheet.csv")
+			const file = fs.readFileSync("sheet.csv", "UTF-8")
+			if (file.indexOf("<!DOCTYPE html>") >= 0) {
+				throw "Could not download csv, maybe csv is not public."
+			}
+			let data = (Papa.parse(file)).data
+			let column = 0
+			if (columnName) {
+				for (var i = 0; i < data[0].length; i++) {
+					if (data[0][i] === columnName) {
+						column = i
+						break
+					}
+				}
+				if (column !== i) {
+					throw `No title ${columnName} in csv file.`
+				}
+				data.shift()
+			}
+			const result = data.map(line => line[column])
+			if (printLogs) {
+				this.log(`Got ${result.length} lines from csv.`, "done")
+			}
+			return result
+		} else {
+			throw `${url} is not a valid URL.`
+		}
+	}
+
+	// Tells the script if it should exit or not, based on execution time left
+	async checkTimeLeft() {
+		const buster = this.buster
+
+		let timeLeft
+		try {
+			timeLeft = await buster.getTimeLeft()
+		} catch (e) {
+			return { timeLeft: true, message: 1000 } // call to getTimeLeft() failed, this is not a reason to abort
+		}
+
+		// the first successful time check is used to detect if we're in a "long running" or "short running" bot
+		if (!this.minTimeBeforeExit) {
+			// a 15min+ time limit is considered a "long running" bot
+			if (timeLeft > 15 * 60) {
+				this.minTimeBeforeExit = 3 * 60 // exit 3 minutes before the end for a "long running" bot
+			} else {
+				this.minTimeBeforeExit = 30 // exit 30s before the end for a "short running" bot
+			}
+		}
+
+		if (timeLeft === -1) {
+			return { timeLeft: false, message: "Script aborted by user." }
+		} else if (timeLeft <= this.minTimeBeforeExit) {
+			return { timeLeft: false, message: `Less than ${this.minTimeBeforeExit} seconds left.` }
+		} else {
+			return { timeLeft: true, message: timeLeft }
+		}
+	}
+
+	// XXX NOTE: contrary to saveResult() this method doesn't call nick.exit()
+	async saveResults(jsonResult, csvResult, name = "result", schema) {
+		const buster = this.buster
+		this.log("Saving data...", "loading")
+		if (schema) {
+			const newResult = []
+			for (let i = 0; i < csvResult.length; i++) {
+				const newItem = {}
+				for (const value of schema) {
+					if (csvResult[i][value]) {
+						newItem[value] = csvResult[i][value]
+					} else {
+						newItem[value] = ""
+					}
+				}
+				newResult.push(newItem)
+			}
+			csvResult = newResult
+		}
+		const csvUrl = await buster.saveText(await jsonexport(csvResult), name + ".csv")
+		this.log(`CSV saved at ${csvUrl}`, "done")
+		const jsonUrl = await buster.saveText(JSON.stringify(jsonResult), name + ".json")
+		this.log(`JSON saved at ${jsonUrl}`, "done")
+		try {
+			await buster.setResultObject(jsonResult)
+		} catch (error) {
+			await buster.setResultObject({ csvUrl, jsonUrl })
+		}
+		this.log("Data successfully saved!", "done")
+		if (this.test) {
+			this.output += "|END|"
+			this._testResult(csvResult)
+			console.log("Test succeed: ended with output:\n" + this.output)
+		}
+	}
+
+	// Function to check if the result is correct for test purposes
+	_testResult(result) {
+		const minLength = this.testRunObject.resMinLength || this.testRunObject.minResLength
+		const desiredOutput = this.testRunObject.desiredOutput
+		if (minLength) {
+			if (result.length < minLength) {
+				console.log(`Test failed: Result has ${result.length} entries but minimum allowed to pass is ${minLength}.`)
+				this.nick.exit(1)
+			}
+		}
+		if (desiredOutput) {
+			let last = 0
+			for (const word of desiredOutput) {
+				const pos = this.output.indexOf(word)
+				if (pos === -1) {
+					console.log(`Test failed: Could not find ${word} in the output.`)
+					this.nick.exit(1)
+				}
+				if (pos < last) {
+					console.log(`Test failed: Could not find ${word} in the right order.`)
+					this.nick.exit(1)
+				} else {
+					last = pos
+				}
+			}
+		}
+	}
+
+	// Function to save an object to csv and in result object if it fits
+	// (DEPRECATED, use saveResults() instead)
+	// XXX NOTE: this function calls nick.exit()
+	async saveResult(result, csvName = "result", schema) {
+		const buster = this.buster
+		this.log("Saving data...", "loading")
+		if (schema) {
+			const newResult = []
+			for (let i = 0; i < result.length; i++) {
+				const newItem = {}
+				for (const value of schema) {
+					if (result[i][value]) {
+						newItem[value] = result[i][value]
+					} else {
+						newItem[value] = ""
+					}
+				}
+				newResult.push(newItem)
+			}
+			result = newResult
+		}
+		const url = await buster.saveText(await jsonexport(result), csvName + ".csv")
+		this.log(`CSV saved at ${url}`, "done")
+		try {
+			await buster.setResultObject(result)
+		} catch (error) {
+			await buster.setResultObject({ csvUrl: url })
+		}
+		this.log("Data successfully saved!", "done")
+		if (this.test) {
+			this.output += "|END|"
+			this._testResult(result)
+			console.log("Test succeed: ended with output:\n" + this.output)
+		}
+		this.nick.exit()
+	}
+
 	// Old way of checking arguments
+	// DEPRECATED
 	checkArguments(args) {
 		const buster = this.buster
 		const finalArgs = []
@@ -109,162 +289,6 @@ class StoreUtilities {
 		return finalArgs
 	}
 
-	// Function to get data from a google spreadsheet or from a csv
-	async getDataFromCsv(url, columnName, printLogs = true) {
-		const buster = this.buster
-		if (printLogs) {
-			this.log(`Getting data from ${url}...`, "loading")
-		}
-		const urlRegex = /^((http[s]?|ftp):\/)?\/?([^:\/\s]+)(:([^\/]*))?((\/[\w\/-]+)*\/)([\w\-\.]+[^#?\s]+)(\?([^#]*))?(#(.*))?$/
-		const match = url.match(urlRegex)
-		if (match) {
-			if (match[3] === "docs.google.com") {
-				if (match[8] === "edit") {
-					url = `https://docs.google.com/${match[6]}export?format=csv`
-				} else {
-					url = `https://docs.google.com/spreadsheets/d/${match[8].replace(/\/$/, "")}/export?format=csv`
-				}
-			}
-			await buster.download(url, "sheet.csv")
-			const file = fs.readFileSync("sheet.csv", "UTF-8")
-			if (file.indexOf("<!DOCTYPE html>") >= 0) {
-				throw "Could not download csv, maybe csv is not public."
-			}
-			let data = (Papa.parse(file)).data
-			let column = 0
-			if (columnName) {
-				for (var i = 0; i < data[0].length; i++) {
-					if (data[0][i] === columnName) {
-						column = i
-						break
-					}
-				}
-				if (column !== i) {
-					throw `No title ${columnName} in csv file.`
-				}
-				data.shift()
-			}
-			const result = data.map(line => line[column])
-			if (printLogs) {
-				this.log(`Got ${result.length} lines from csv.`, "done")
-			}
-			return result
-		} else {
-			throw `${url} is not a valid URL.`
-		}
-	}
-
-	// Function to check if the result is correct for test purpose
-	_testResult(result) {
-		const minLength = this.testRunObject.resMinLength || this.testRunObject.minResLength
-		const desiredOutput = this.testRunObject.desiredOutput
-		if (minLength) {
-			if (result.length < minLength) {
-				console.log(`Test failed: Result has ${result.length} entries but minimum allowed to pass is ${minLength}.`)
-				this.nick.exit(1)
-			}
-		}
-		if (desiredOutput) {
-			let last = 0
-			for (const word of desiredOutput) {
-				const pos = this.output.indexOf(word)
-				if (pos === -1) {
-					console.log(`Test failed: Could not find ${word} in the output.`)
-					this.nick.exit(1)
-				}
-				if (pos < last) {
-					console.log(`Test failed: Could not find ${word} in the right order.`)
-					this.nick.exit(1)
-				} else {
-					last = pos
-				}
-			}
-		}
-	}
-
-	// Function to save the result if there is no time or if the user shift Stop the script
-	async checkTimeLeft() {
-		const buster = this.buster
-		const timeLeft = await buster.getTimeLeft()
-		if (timeLeft === -1 || timeLeft < 30) {
-			const reason = timeLeft === -1 ? "Script aborted by user." : "Less than 30 seconds left."
-			return { timeLeft: false, message: reason }
-		} else {
-			return { timeLeft: true, message: timeLeft }
-		}
-	}
-
-	async saveResults(jsonResult, csvResult, name = "result", schema) {
-		const buster = this.buster
-		this.log("Saving data...", "loading")
-		if (schema) {
-			const newResult = []
-			for (let i = 0; i < csvResult.length; i++) {
-				const newItem = {}
-				for (const value of schema) {
-					if (csvResult[i][value]) {
-						newItem[value] = csvResult[i][value]
-					} else {
-						newItem[value] = ""
-					}
-				}
-				newResult.push(newItem)
-			}
-			csvResult = newResult
-		}
-		const csvUrl = await buster.saveText(await jsonexport(csvResult), name + ".csv")
-		this.log(`CSV saved at ${csvUrl}`, "done")
-		const jsonUrl = await buster.saveText(JSON.stringify(jsonResult), name + ".json")
-		this.log(`JSON saved at ${jsonUrl}`, "done")
-		try {
-			await buster.setResultObject(jsonResult)
-		} catch (error) {
-			await buster.setResultObject({ csvUrl, jsonUrl })
-		}
-		this.log("Data successfully saved!", "done")
-		if (this.test) {
-			this.output += "|END|"
-			this._testResult(csvResult)
-			console.log("Test succeed: ended with output:\n" + this.output)
-		}
-	}
-
-	// Function to save an object to csv and in result object if it fits
-	// (deprecated, use saveResults() instead)
-	// XXX NOTE: this function calls nick.exit()
-	async saveResult(result, csvName = "result", schema) {
-		const buster = this.buster
-		this.log("Saving data...", "loading")
-		if (schema) {
-			const newResult = []
-			for (let i = 0; i < result.length; i++) {
-				const newItem = {}
-				for (const value of schema) {
-					if (result[i][value]) {
-						newItem[value] = result[i][value]
-					} else {
-						newItem[value] = ""
-					}
-				}
-				newResult.push(newItem)
-			}
-			result = newResult
-		}
-		const url = await buster.saveText(await jsonexport(result), csvName + ".csv")
-		this.log(`CSV saved at ${url}`, "done")
-		try {
-			await buster.setResultObject(result)
-		} catch (error) {
-			await buster.setResultObject({ csvUrl: url })
-		}
-		this.log("Data successfully saved!", "done")
-		if (this.test) {
-			this.output += "|END|"
-			this._testResult(result)
-			console.log("Test succeed: ended with output:\n" + this.output)
-		}
-		this.nick.exit()
-	}
 }
 
 module.exports = StoreUtilities
