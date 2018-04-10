5fb5049de2b594d7225200fb8e979a4c61319136
Martin Barreau <martin.barreau@gmail.com>
2018-04-10 16:39:25 +0200

    diff.sh can now be used on Mac OS X (colordiff required)

--- diff.sh
+++ diff.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env zsh
 
+unamestr=`uname`
+
+if [[ "$unamestr" == 'Linux' ]]; then
+  diffcmd='diff --color'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+  diffcmd='colordiff'
+else
+  echo "Could not determine OS / diff binary"
+  exit 1
+fi
+
 cd libs
 for i in *(.)
 do
   dev=dev/`echo $i | sed 's/\.js/-DEV\.js/g'`
-  diff --color --context=2 $i $dev
+  $diffcmd --context=2 $i $dev
 done
 cd ..
 
@@ -12,6 +23,6 @@ cd store
 for i in *(.)
 do
   dev=dev/`echo $i | sed 's/\.js/ DEV\.js/g'`
-  diff --color --context=2 $i $dev
+  $diffcmd --context=2 $i $dev
 done
 cd ..
