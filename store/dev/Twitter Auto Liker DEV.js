86ed04965ebec33f26e4ddbcfa15d1cf310f43ab
Martin Barreau <martin.barreau@gmail.com>
2018-04-13 16:35:51 +0200

    Twitter Auto Liker DEV
    New API which can like a given count of tweets from profiles, or all tweets from a profile

---
+++ store/dev/Twitter Auto Liker DEV.js
@@ -0,0 +1,232 @@
+// Phantombuster configuration {
+"phantombuster command: nodejs"
+"phantombuster package: 5"
+"phantombuster dependencies: lib-StoreUtilities.js"
+"phantombuster flags: save-folder"
+	
+const url = require("url")
+const Buster = require("phantombuster")
+const buster = new Buster()
+	
+const Nick = require("nickjs")
+const nick = new Nick({
+	loadImages: false,
+	printPageErrors: false,
+	printResourceErrors: false,
+	printNavigation: false,
+	printAborts: false,
+	debug: false,
+})
+	
+const StoreUtilities = require("./lib-StoreUtilities")
+const utils = new StoreUtilities(nick, buster)
+// }
+
+/**
+ * @description Get username of a twitter account
+ * @param {Object} arg
+ * @param {Function} callback
+ */
+const scrapeUserName = (arg, callback) => {
+	callback(null, document.querySelector(".DashboardProfileCard-name a").textContent.trim())
+}
+
+/**
+ * @description Connects to twitter with a session ID
+ * @param {Object} tab Nick tab in use
+ * @param {String} sessionCookie Your session cookie for twitter
+ */
+const twitterConnect = async (tab, sessionCookie) => {
+	utils.log("Connecting to Twitter...", "loading")
+	try {
+		await nick.setCookie({
+			name: "auth_token",
+			value: sessionCookie,
+			domain: ".twitter.com",
+			httpOnly: true,
+			secure: true
+		})
+		await tab.open("https://twitter.com/")
+		await tab.waitUntilVisible(".DashboardProfileCard")
+		utils.log(`Connected as ${await tab.evaluate(scrapeUserName)}`, "done")
+	} catch (error) {
+		utils.log("Could not connect to Twitter with this sessionCookie.", "error")
+		nick.exit(1)
+	}
+}
+
+/**
+ * @description
+ * @param {Object} arg - Arguments to pass in browser context
+ * @param {Function} cb - Function to quit browser context
+ * @return {Promise<Number>} Tweets liked count
+ */
+const likeTweets = (arg, cb) => {
+	const tweetsLoaded = Array.from(document.querySelectorAll("div.tweet.js-actionable-tweet"))
+
+	/**
+	 * NOTE: If the script loaded more tweets than likesCount, then we remove trailing tweets to get the exact count
+	 */
+	if (tweetsLoaded.length > arg.likesCount) {
+		tweetsLoaded.splice(arg.likesCount - tweetsLoaded.length)
+	}
+	for (const one of tweetsLoaded) {
+		one.querySelector(".HeartAnimation").click()
+	}
+	cb(null, tweetsLoaded.length)
+}
+
+/**
+ * @description
+ * @param {Object} arg - Arguments passed from node context to browser context
+ * @param {Function} cb - Function used to quit the browser context
+ * @return {Promise<Number>} Tweets count from a profile
+ */
+const getTweetsCount = (arg, cb) => {
+	// Easy way to get the tweets count for a profile
+	const countSelector = "a[data-nav=\"tweets\"] > span.ProfileNav-value"
+	const anchor = document.querySelector(countSelector)
+	cb(null, anchor ? parseInt(anchor.dataset.count, 10) : 0)
+}
+
+/**
+ * NOTE: This function is different from getTweetsCount
+ * getTweetsCount returns the total tweets count from a profile
+ * This function returns the amount of tweets loaded in the browser
+ * @description
+ * @param {Object} arg - Arguments passed from node context to browser context
+ * @param {Function} cb - Function used to quit the browser context
+ * @return {Promise<Number>} Tweets count loaded at screen
+ */
+const getLoadedTweetsCount = (arg, cb) => {
+	cb(null, Array.from(document.querySelectorAll("div.tweet.js-actionable-tweet")).length)
+}
+
+/**
+ * @description Function used to open a profile and like a certain amount of tweets
+ * @throws if there were an error during when opening the profile or during the like procedure
+ * @param {Object} tab - Nickjs tab object
+ * @param {String} profile - url or profile name to open
+ * @param {Number} [likesCount] - Total count of tweets to like in the given profile (default 20)
+ * @return {Promise<Object>}
+ */
+const loadProfileAndLike = async (tab, profile, likesCount = 20) => {
+	const url = isUrl(profile) ? profile : `https://twitter.com/${profile}`
+
+	const [httpCode] = await tab.open(url)
+	if (httpCode === 404) {
+		throw `Cannot open the URL: ${url}`
+	}
+	await tab.waitUntilVisible(".ProfileHeading", 7500)
+	const tweetsCount = await tab.evaluate(getTweetsCount)
+	/**
+	 * NOTE: If the likeCount parameter is bigger than the total tweets count, the script will like every tweets
+	 */
+	if (likesCount > tweetsCount) {
+		likesCount = tweetsCount
+	}
+	utils.log(`${tweetsCount} tweets found from ${url}`, "info")
+	utils.log(`Now loading ${likesCount} tweets ...`, "loading")
+	let loadedCount = await tab.evaluate(getLoadedTweetsCount)
+
+	while (loadedCount < likesCount) {
+		loadedCount = await tab.evaluate(getLoadedTweetsCount)
+		buster.progressHint(loadedCount / likesCount, `Tweets loaded: ${loadedCount}/${likesCount}`)
+		await tab.scrollToBottom()
+		/**
+		 * HACK: Since we don't load images with Nick.js to run the script faster,
+		 * we loose the tweets loading spinner, since we can now how many tweets are loaded at screen
+		 * we just wait that there is more tweets at screen
+		 */
+		try {
+			const res = await tab.evaluate((arg, cb) => {
+				const startingTimestamp = Date.now()
+				const waitForNewTweets = () => {
+					const loadedTweets = Array.from(document.querySelectorAll("div.tweet.js-actionable-tweet")).length
+					if (loadedTweets <= arg.previousTweetsCount) {
+						if (Date.now() - startingTimestamp >= 30000) {
+							cb("Tweets cannot be loaded after waiting 30s")
+						}
+						setTimeout(waitForNewTweets, 100)
+					} else if (!document.querySelector(".stream-container").dataset.minPosition) {
+						cb(true)
+					} else {
+						cb(null)
+					}
+				}
+				waitForNewTweets()
+			}, { previousTweetsCount: loadedCount })
+		} catch (err) {
+			utils.log(`Error during tweets loading: ${err.message || err}`, "info")
+			break
+		}
+	}
+	utils.log(`Liking ${likesCount}`, "info")
+	const likeCount = await tab.evaluate(likeTweets, { likesCount })
+	return { url, likeCount }
+}
+
+/**
+ * @description Tiny function used to check if a given string represents an URL
+ * @param {String} target
+ * @return { Boolean } true if target represents an URL otherwise false
+ */
+const isUrl = target => url.parse(target).hostname !== null
+
+/**
+ * @description Tiny function used tio check if a given string represents a Twitter URL
+ * @param { String } target 
+ * @return { Boolean } true if target represents an Twitter URL otherwise false
+ */
+const isTwitterUrl = target => url.parse(target).hostname === "twitter.com"
+
+/**
+ * @description Main function to launch everything
+ */
+;(async () => {
+	const tab = await nick.newTab()
+	const result = []
+	let likedCount = 0
+	let {spreadsheetUrl, columnName, csvName, queries, sessionCookie, likesCountPerProfile } = utils.validateArguments()
+
+	if (!csvName) {
+		csvName = "result"
+	}
+
+	if (spreadsheetUrl) {
+		if (isUrl(spreadsheetUrl)) {
+			queries = await utils.getDataFromCsv(spreadsheetUrl, columnName)
+		} else if (typeof spreadsheetUrl === "string") {
+			queries = [ (isTwitterUrl(spreadsheetUrl)) ? spreadsheetUrl : `https://twitter.com/${spreadsheetUrl}` ]
+		}
+	}
+
+	await twitterConnect(tab, sessionCookie)
+
+	for (const profile of queries) {
+		const timeLeft = await utils.checkTimeLeft()
+		if (!timeLeft.timeLeft) {
+			utils.log(timeLeft.message, "warning")
+			break
+		}
+		try {
+			let profileLiked = null
+			profileLiked = await loadProfileAndLike(tab, profile, likesCountPerProfile)
+			likedCount += profileLiked.likeCount
+			result.push(profileLiked)
+		} catch (err) {
+			utils.log(`Cannot like ${profile} due to: ${err.message || err}`, "error")
+			utils.log(`Dumping error stack: ${err.stack || ""}`, "error")
+			await tab.screenshot(`${Date.now()}.jpg`)
+			continue
+		}
+	}
+
+	utils.log(`${likedCount} tweets liked for ${result.length} profiles`, "done")
+	await utils.saveResults(result, result, csvName)
+	nick.exit()
+})()
+	.catch(err => {
+		utils.log(err, "error")
+		nick.exit(1)
+	})
