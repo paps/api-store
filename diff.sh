72780b3ace2f305a0b42afe4d7ec863ad27888c5
Martin Tapia <contact@martintapia.com>
2018-04-25 16:54:09 +0200

    update diff script to handle new directory structure

--- diff.sh
+++ diff.sh
@@ -3,26 +3,33 @@
 unamestr=`uname`
 
 if [[ "$unamestr" == 'Linux' ]]; then
-  diffcmd='diff --color'
+  diffcmd='diff'
+  diffarg='--color'
 elif [[ "$unamestr" == 'Darwin' ]]; then
   diffcmd='colordiff'
+  diffarg=''
 else
   echo "Could not determine OS / diff binary"
   exit 1
 fi
 
 cd libs
-for i in *(.)
-do
-  dev=dev/`echo $i | sed 's/\.js/-DEV\.js/g'`
-  $diffcmd --context=2 $i $dev
+for i in *(.); do
+  dev=DEV/`echo $i | sed 's/\.js$/-DEV\.js/'`
+  $diffcmd $diffarg --context=2 "$i" "$dev"
 done
 cd ..
 
 cd store
-for i in *(.)
-do
-  dev=dev/`echo $i | sed 's/\.js/ DEV\.js/g'`
-  $diffcmd --context=2 $i $dev
+for i in */; do
+  if [[ "$i" != "DEV/" ]]; then
+    jsprod=$i`echo $i | sed 's/\/$/.js/'`
+    jsdev=DEV/`echo $i | sed 's/\/$/ DEV/'`/`echo $i | sed 's/\/$/ DEV.js/'`
+    jsonprod=$i`echo $i | sed 's/\/$/.json/'`
+    jsondev=DEV/`echo $i | sed 's/\/$/ DEV/'`/`echo $i | sed 's/\/$/ DEV.json/'`
+    mdprod=$i`echo $i | sed 's/\/$/.md/'`
+    mddev=DEV/`echo $i | sed 's/\/$/ DEV/'`/`echo $i | sed 's/\/$/ DEV.md/'`
+    $diffcmd $diffarg --context=2 "$jsprod" "$jsdev"
+  fi
 done
 cd ..
