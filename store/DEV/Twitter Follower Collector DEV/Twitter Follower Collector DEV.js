25168789995771f676391583ef9a0d88527d2261
Martin Barreau <martin.barreau@gmail.com>
2018-05-09 14:20:34 +0200

    Twitter Follower Collector DEV: Issue #47
    The DEV version of the API has now the ability to collect a given amount of followers from input profiles

--- store/DEV/Twitter Follower Collector DEV/Twitter Follower Collector DEV.js
+++ store/DEV/Twitter Follower Collector DEV/Twitter Follower Collector DEV.js
@@ -19,6 +19,7 @@ const nick = new Nick({
 
 const StoreUtilities = require("./lib-StoreUtilities")
 const utils = new StoreUtilities(nick, buster)
+const MAX_FOLLOWERS_PER_ACCOUNT = -1
 // }
 
 const scrapeUserName = (arg, callback) => {
@@ -45,7 +46,7 @@ const twitterConnect = async (tab, sessionCookie) => {
 }
 
 const getFollowersNb = (arg, callback) => {
-	callback(null, document.querySelectorAll(`div.GridTimeline div[data-test-selector="ProfileTimelineUser"]`).length)
+	callback(null, document.querySelectorAll("div.GridTimeline div[data-test-selector=\"ProfileTimelineUser\"]").length)
 }
 
 const getDivsNb = (arg, callback) => {
@@ -53,7 +54,7 @@ const getDivsNb = (arg, callback) => {
 }
 
 const scrapeFollowers = (arg, callback) => {
-	const followers = document.querySelectorAll(`div.Grid-cell[data-test-selector="ProfileTimelineUser"]`)
+	const followers = document.querySelectorAll("div.Grid-cell[data-test-selector=\"ProfileTimelineUser\"]")
 
 	const results = []
 
@@ -67,7 +68,7 @@ const scrapeFollowers = (arg, callback) => {
 	callback(null, results)
 }
 
-const getTwitterFollowers = async (tab, twitterHandle) => {
+const getTwitterFollowers = async (tab, twitterHandle,  followersPerAccount) => {
 	utils.log(`Getting followers for ${twitterHandle}`, "loading")
 	if (twitterHandle.match(/twitter\.com\/([A-z0-9\_]+)/)) {
 		twitterHandle = twitterHandle.match(/twitter\.com\/([A-z0-9\_]+)/)[1]
@@ -82,6 +83,16 @@ const getTwitterFollowers = async (tab, twitterHandle) => {
 			utils.log(`Stopped getting followers for ${twitterHandle}: ${timeLeft.message}`, "warning")
 			break
 		}
+		/**
+		 * NOTE: Issue #47 Ability to limit the total number of profiles collected
+		 */
+		if (followersPerAccount > 0) {
+			if (await tab.evaluate(getFollowersNb) >= followersPerAccount) {
+				utils.log(`Loaded ${await tab.evaluate(getFollowersNb)} followers.`, "done")
+				loop = false
+				continue
+			}
+		}
 		await tab.scrollToBottom()
 		try {
 			await tab.waitUntilVisible(`div.GridTimeline-items > div.Grid:nth-child(${n+1})`)
@@ -92,8 +103,18 @@ const getTwitterFollowers = async (tab, twitterHandle) => {
 			loop = false
 		}
 	}
-	const followers = await tab.evaluate(scrapeFollowers)
-	utils.log(`Scraped all followers for ${twitterHandle}`, "done")
+	let followers = await tab.evaluate(scrapeFollowers)
+
+	if (followersPerAccount > 0) {
+		if (followersPerAccount < followers.length) {
+			followers = followers.splice(0, followersPerAccount)
+			utils.log(`Scraped ${followersPerAccount} followers for ${twitterHandle}`, "done")
+		} else {
+			utils.log(`Scraped ${followers.length} followers for ${twitterHandle}`, "done")
+		}
+	} else {
+		utils.log(`Scraped all followers for ${twitterHandle}`, "done")
+	}
 	return followers
 }
 
@@ -110,7 +131,12 @@ const jsonToCsv = json => {
 
 ;(async () => {
 	const tab = await nick.newTab()
-	let {spreadsheetUrl, sessionCookie} = utils.validateArguments()
+	let {spreadsheetUrl, sessionCookie, followersPerAccount} = utils.validateArguments()
+
+	if (!followersPerAccount) {
+		followersPerAccount = MAX_FOLLOWERS_PER_ACCOUNT
+	}
+
 	await twitterConnect(tab, sessionCookie)
 	let twitterUrls = [spreadsheetUrl]
 	if (spreadsheetUrl.indexOf("docs.google.com") > -1) {
@@ -125,7 +151,7 @@ const jsonToCsv = json => {
 				utils.log(`Script stopped: ${timeLeft.message}`, "warning")
 				break
 			}
-			const followers = await getTwitterFollowers(tab, twitterUrl)
+			const followers = await getTwitterFollowers(tab, twitterUrl, followersPerAccount)
 			const newJson = {isFollowing: twitterUrl, followers}
 			const newCsv = jsonToCsv(newJson)
 			csvResult = csvResult.concat(newCsv)
@@ -135,7 +161,7 @@ const jsonToCsv = json => {
 	await utils.saveResults(jsonResult, csvResult, "result", ["profileUrl", "name", "bio", "isFollowing"])
 	nick.exit()
 })()
-.catch(err => {
-	utils.log(err, "error")
-	nick.exit(1)
-})
+	.catch(err => {
+		utils.log(err, "error")
+		nick.exit(1)
+	})
