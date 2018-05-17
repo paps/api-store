27e0a27f5e8cafa67eacfbf474cb54fe653fda7d
Martin Barreau <martin.barreau@gmail.com>
2018-05-17 17:52:47 +0200

    Twitter Auto Liker DEV:
    Non printables characters on Twitter profiles URLs are now removed

--- store/DEV/Twitter Follower Collector DEV/Twitter Follower Collector DEV.js
+++ store/DEV/Twitter Follower Collector DEV/Twitter Follower Collector DEV.js
@@ -2,7 +2,6 @@
 "phantombuster command: nodejs"
 "phantombuster package: 4"
 "phantombuster dependencies: lib-StoreUtilities.js"
-"phantombuster flags: save-folder"
 
 const Buster = require("phantombuster")
 const buster = new Buster()
