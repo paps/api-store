2d76e13c075dbaf8a3f0518a2552b24f0f6a3833
Nathan Gerszanowilsz <nathan.gerszanowilsz@phantombuster.com>
2018-07-13 18:29:02 +0200

    a few modifications of code cleaning

--- store/DEV/Facebook Group Extractor DEV/Facebook Group Extractor DEV.js
+++ store/DEV/Facebook Group Extractor DEV/Facebook Group Extractor DEV.js
@@ -10,7 +10,7 @@ const buster = new Buster()
 
 const Nick = require("nickjs")
 const nick = new Nick({
-	loadImages: true,
+	loadImages: false,
 	printPageErrors: false,
 	printResourceErrors: false,
 	printNavigation: false,
@@ -35,14 +35,13 @@ const isFacebookGroupURL = (targetUrl) => {
 	return 1
 }
 
-const filterResult = ( arr, key ) => {
-    // let tempArray = []
+const removeDuplicates = (arr, key) => {
     let resultArray = []
-    for (let i = 0; i < arr.length ; i++){
-        if (!resultArray.find(el => el[key]===arr[i][key])){
+    for (let i = 0; i < arr.length ; i++) {
+        if (!resultArray.find(el => el[key] === arr[i][key])) {
             resultArray.push(arr[i])
         } else {
-            let index = resultArray.findIndex(el => el[key]===arr[i][key])
+            let index = resultArray.findIndex(el => el[key] === arr[i][key])
             resultArray[index].firstLine = arr[i].firstLine
             resultArray[index].secondLine = arr[i].secondLine
             resultArray[index].inCommon = arr[i].inCommon
@@ -63,20 +62,24 @@ const scrapeMainPage = (arg, callback) => {
 	for (const result of results) {
         const url = result.querySelector("a").href
         // a few profiles don't have a name and are just www.facebook.com/profile.php?id=ID_NUMBER&fref..
-        let profileURL = (url.indexOf("profile.php?") > -1) ?  url.slice(0,url.indexOf("&")) :  url.slice(0, url.indexOf("?"))
+        let profileURL = (url.indexOf("profile.php?") > -1) ? url.slice(0,url.indexOf("&")) : url.slice(0, url.indexOf("?"))
         let newInfos = { profileURL }
         newInfos.imageURL = result.querySelector("img").src
         newInfos.name = result.querySelector("img").getAttribute("aria-label")
-        if (result.querySelector(".friendButton")) newInfos.category = "Friend"
+        if (result.querySelector(".friendButton")) {
+            newInfos.category = "Friend"
+        }
         let dateAndJob = result.querySelectorAll("._60rj")
         for (let data of dateAndJob){
             if (newInfos.firstLine) {
-                newInfos.secondLine = data.textContent
+                newInfos.secondLine = data.textContent.trim()
             } else { 
-                newInfos.firstLine = data.textContent
+                newInfos.firstLine = data.textContent.trim()
             }
         }
-        if (result.querySelector("._2zy5 a")) newInfos.inCommon = result.querySelector("._2zy5 a").textContent 
+        if (result.querySelector("._2zy5 a")) {
+            newInfos.inCommon = result.querySelector("._2zy5 a").textContent.trim() 
+        }
         infos.push(newInfos)
     }
     // let infos = [{ membersNumber, adminsNumber, friendsNumber, groupName, cleanUrl}]
@@ -100,7 +103,7 @@ const getGroupResult = async (tab, groupUrl, query) => {
     let moreToLoad
     do{
         await tab.scrollToBottom()
-        await tab.wait(200)
+        // await tab.wait(200)
         moreToLoad = await tab.evaluate((arg, callback) => {
             callback(null, document.querySelector(".clearfix.mam.uiMorePager.stat_elem.morePager"))
         })
@@ -135,21 +138,22 @@ nick.newTab().then(async (tab) => {
             if (url.slice(-1) !== "/") url += "/"  // Add a trailing slash if none
 
             const query = queryColumn ? membersURL : false
-            result = result.concat(await getGroupResult(tab, url+"members", query))
-            result = result.concat(await getGroupResult(tab, url+"admins", query))
-            result = result.concat(await getGroupResult(tab, url+"friends", query))
-            result = result.concat(await getGroupResult(tab, url+"members_with_things_in_common", query))
-            result = result.concat(await getGroupResult(tab, url+"local_members", query))
+            result = result.concat(await getGroupResult(tab, url + "members", query))
+            result = result.concat(await getGroupResult(tab, url + "admins", query))
+            result = result.concat(await getGroupResult(tab, url + "friends", query))
+            result = result.concat(await getGroupResult(tab, url + "members_with_things_in_common", query))
+            result = result.concat(await getGroupResult(tab, url + "local_members", query))
 
 		} else {  
 			utils.log(`${url} doesn't constitute a Facebook Group URL... skipping entry`, "warning")
 			continue
 		}
     }
-    // await utils.saveResults(result, craftCsvObject(result), "resultsFB")
-    console.log("avant", result)
-    const results = filterResult(result, "profileURL")
-	await utils.saveResult(results)
+
+    const finalResult = removeDuplicates(result, "profileURL")
+
+    await utils.saveResults(finalResult, finalResult, "resultsFB")
+	// await utils.saveResult(results)
  
     
     utils.log("Job is done!", "done")
