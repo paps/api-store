055ba70308fc1a452bacbbe2220187076377ea05
Nathan Gerszanowilsz <nathan.gerszanowilsz@phantombuster.com>
2018-07-26 16:15:02 +0200

    I Flwing C new API dev&prod

---
+++ store/Instagram Following Collector/Instagram Following Collector.js
@@ -0,0 +1,258 @@
+// Phantombuster configuration {
+"phantombuster command: nodejs"
+"phantombuster package: 5"
+"phantombuster dependencies: lib-StoreUtilities.js, lib-Instagram.js"
+
+const Buster = require("phantombuster")
+const buster = new Buster()
+
+const Nick = require("nickjs")
+const nick = new Nick({
+	loadImages: false,
+	printPageErrors: false,
+	printResourceErrors: false,
+	printNavigation: false,
+	printAborts: false,
+	debug: false,
+})
+
+const StoreUtilities = require("./lib-StoreUtilities")
+const utils = new StoreUtilities(nick, buster)
+const Instagram = require("./lib-Instagram")
+const instagram = new Instagram(nick, buster, utils)
+const { parse } = require("url")
+// }
+let rateLimitReached = 0
+
+const getUrlsToScrape = (data, numberofProfilesperLaunch) => {
+	data = data.filter((item, pos) => data.indexOf(item) === pos)
+	const maxLength = data.length
+	if (maxLength === 0) {
+		utils.log("Input spreadsheet is empty OR we already scraped all the profiles from this spreadsheet.", "warning")
+		nick.exit()
+	}
+	return data.slice(0, Math.min(numberofProfilesperLaunch, maxLength)) // return the first elements
+}
+
+// Checks if a url is already in the csv
+const checkDb = (str, db) => {
+	for (const line of db) {
+		if (str === line.followedBy) {
+			return false
+		}
+	}   
+    return true
+}
+
+const cleanInstagramUrl = (url) => {
+	if (url && url.includes("instagram.")) {
+		let path = parse(url).pathname
+		path = path.slice(1)
+		const id = path.slice(0, path.indexOf("/"))
+		if (id !== "p") { /// not a picture url
+			return "https://www.instagram.com/" + id 
+		}
+	}
+	return null
+}
+
+// Removes any duplicate profile 
+const removeDuplicates = (arr) => {
+	let resultArray = []
+	for (let i = 0; i < arr.length ; i++) {
+		if (!resultArray.find(el => el.profileName === arr[i].profileName && el.followedBy === arr[i].followedBy)) {
+			resultArray.push(arr[i])
+		}
+	}
+	return resultArray
+}
+
+const scrapeFollowingCount = (arg, callback) => {
+	let followingCount = 0
+	if (document.querySelector("main ul li:nth-child(3) span > span")) {
+		followingCount = document.querySelector("main ul li:nth-child(3) span > span").textContent
+	} else {
+		followingCount = document.querySelector("main ul li:nth-child(3) span").textContent
+	}
+	followingCount = parseInt(followingCount.replace(/,/g, ""), 10)
+	callback(null, followingCount)
+}
+
+const scrape = (arg, callback) => {
+	const results = document.querySelectorAll("body > div:last-child > div > div:last-of-type > div > div:last-child > ul li")
+
+	const data = []
+	let profilesScraped = 0
+	for (const result of results) {
+		if (result.querySelector("div > div")) {
+			const pictureUrl = result.querySelector("div > div a img").src
+			
+			let newInfos = { pictureUrl }
+			if (result.querySelector("div > div > div > div")) {
+				newInfos.profileUrl = result.querySelector("div > div > div > div > a").href
+				newInfos.profileName = result.querySelector("div > div > div > div").textContent
+			}
+			if (result.querySelector("div > div > div > div:last-child")) {
+				newInfos.fullName = result.querySelector("div > div > div > div:last-child").textContent
+			}
+			newInfos.followedBy = arg.url
+			const button = result.querySelector("button").textContent
+			if (button === "Following") { newInfos.followedByUser = "Followed By User" }
+			data.push(newInfos)
+			if (++profilesScraped >= arg.numberMaxOfFollowing) { break }
+		}
+    } 
+	callback(null, data)
+}
+
+const getFollowing = async (tab, url, numberMaxOfFollowing) => {
+    let result = []
+    try {
+		await tab.click("main ul li:nth-child(3) a")
+        await tab.waitUntilVisible("body > div:last-child > div > div:last-of-type > div > div:last-child > ul li > div > div > div > div:last-child", 7500)
+    } catch (err) {
+		// Hitting Instagram rate limit
+		utils.log("Couldn't load Following list, Instagram rate limit probably reached.", "warning")
+		rateLimitReached++
+        return result
+	}
+	await tab.wait(200)
+	const followingCount = await tab.evaluate(scrapeFollowingCount)
+    let profilesCount = 0 
+    let showMessage = 0
+	let lastScrollDate = new Date()
+	let checkProfilesCount
+    do{
+        try {
+            checkProfilesCount = await tab.evaluate((arg, callback) => {
+                callback(null, document.querySelectorAll("body > div:last-child > div > div:last-of-type > div > div:last-child > ul li").length)
+            })
+
+            if (checkProfilesCount > profilesCount) {
+				await tab.wait(800)
+                showMessage++
+                profilesCount = checkProfilesCount
+                if (showMessage % 15 === 0) { utils.log(`Loaded ${profilesCount} profiles...`, "loading") }
+				buster.progressHint(profilesCount / numberMaxOfFollowing, `${profilesCount} profiles loaded`)
+
+				try {
+					await tab.waitUntilPresent("body > div:last-child > div > div:last-of-type > div > div:last-child > ul li:last-child a", 8000) // if last li element is a profile and not a spinner
+					await tab.evaluate((arg, callback) => { // scrollToBottom function
+						callback(null, document.querySelector("body > div:last-child > div > div:last-of-type > div > div:last-child > ul li:last-child a").scrollIntoView())
+					})
+				} catch (err) {
+					utils.log(`Couldn't fully load Following list, only got ${profilesCount} profiles.`, "warning")
+					break
+				}
+                lastScrollDate = new Date()
+            } else {
+                await tab.wait(100)
+            }
+
+            const timeLeft = await utils.checkTimeLeft()
+            if (!timeLeft.timeLeft) {
+                utils.log(timeLeft.message, "warning")
+                break
+            }
+			if (profilesCount >= followingCount) {
+				utils.log(`Loaded all ${profilesCount} profiles.`, "done")
+				break
+			}
+            if (new Date() - lastScrollDate > 7000) {
+				utils.log(`Scrolling took too long, only got ${profilesCount} profiles.`, "done")
+                break
+			}  
+			
+        } catch (err) {
+			utils.log("Error scrolling down the page", "error") 
+			console.log(err)
+        }
+	} while (checkProfilesCount < numberMaxOfFollowing)
+	if (checkProfilesCount >= numberMaxOfFollowing) {
+		utils.log(`Got the last ${numberMaxOfFollowing} profiles ouf of ${followingCount}.`, "done")
+	}
+	buster.progressHint(1, `${profilesCount} profiles loaded`)
+	await tab.wait(2000)
+    result = result.concat(await tab.evaluate(scrape, { url, numberMaxOfFollowing }))
+    return result
+}
+
+// Main function that execute all the steps to launch the scrape and handle errors
+;(async () => {
+	let { sessionCookie, spreadsheetUrl, columnName, numberMaxOfFollowing, numberofProfilesperLaunch, csvName } = utils.validateArguments()
+	if (!csvName) { csvName = "result" }
+	let urls, result = []
+	result = await utils.getDb(csvName + ".csv")
+	if (!numberMaxOfFollowing) { numberMaxOfFollowing = 100 }
+	if (spreadsheetUrl.toLowerCase().includes("instagram.com/")) { // single instagram url
+		urls = cleanInstagramUrl(utils.adjustUrl(spreadsheetUrl, "instagram"))
+		if (urls) {	
+			urls = [ urls ]
+		} else {
+			utils.log("The given url is not a valid instagram profile url.", "error")
+		}
+	} else { // CSV
+		urls = await utils.getDataFromCsv(spreadsheetUrl, columnName)
+		for (let i = 0; i < urls.length; i++) { // cleaning all instagram entries
+			if (urls[i].startsWith("@")) { // converting @profile_name to https://www.instagram/profile_name
+				urls[i] = "https://www.instagram.com/" + urls[i].slice(1)
+			} else {
+				urls[i] = utils.adjustUrl(urls[i], "instagram")
+				urls[i] = cleanInstagramUrl(urls[i])
+			}
+		}
+		urls = urls.filter(str => str) // removing empty lines
+		if (!numberofProfilesperLaunch) {
+			numberofProfilesperLaunch = urls.length
+		}
+		urls = getUrlsToScrape(urls.filter(el => checkDb(el, result)), numberofProfilesperLaunch)
+	}
+	result = await utils.getDb(csvName + ".csv")
+	
+
+	console.log(`URLs to scrape: ${JSON.stringify(urls, null, 4)}`)
+	const tab = await nick.newTab()
+	await instagram.login(tab, sessionCookie)
+
+	let pageCount = 0
+	for (let url of urls) {
+		const timeLeft = await utils.checkTimeLeft()
+		if (!timeLeft.timeLeft) {
+			utils.log(`Scraping stopped: ${timeLeft.message}`, "warning")
+			break
+		}
+		try {
+			utils.log(`Scraping profiles from ${url}`, "loading")
+			pageCount++
+			buster.progressHint(pageCount / urls.length, `${pageCount} profile${pageCount > 1 ? "s" : ""} scraped`)
+			await tab.open(url)
+			const selected = await tab.waitUntilVisible(["main ul li:nth-child(2) a", ".error-container", "article h2"], 10000, "or")
+			if (selected === ".error-container") {
+				utils.log(`Couldn't open ${url}, broken link or page has been removed.`, "warning")
+				continue
+			} else if (selected === "article h2") {
+				utils.log("Private account, cannot access Following list.", "warning")
+				continue
+			}
+			result = result.concat(await getFollowing(tab, url, numberMaxOfFollowing))
+
+		} catch (err) {
+			utils.log(`Can't scrape the profile at ${url} due to: ${err.message || err}`, "warning")
+			continue
+		}
+		if (rateLimitReached >= 3) {
+			utils.log("Rate limit reached, stopping the agent.", "warning")
+			break
+		}
+	}
+	
+	result = removeDuplicates(result)
+	
+	await utils.saveResults(result, result, csvName, null, false)
+	nick.exit(0)
+	
+})()
+.catch(err => {
+	utils.log(err, "error")
+	nick.exit(1)
+})
---
+++ store/Instagram Following Collector/Instagram Following Collector.md
@@ -0,0 +1,104 @@
+# Find out who is following an Instagram account
+Sometimes it can be useful to know which accounts are followed by a specific one. With this API, choose your own or any Instagram account and get in seconds a clear listing of every account followed by your target.
+
+# What will you need? ⚙️ 
+
+- **Session cookie**: Your session cookie from Instagram.
+- **Spreadsheet URL**: The link of a Google Spreadsheet with Instagram profile URLs in it, or a single specific Instagram URL.
+
+_(**You already have all that?**  Click straight away on **"Use this API"**)_
+
+
+# Which data will you collect?
+
+
+1. Instagram profile link
+1. Instagram username
+1. Full name
+1. Profile image URL
+
+
+# Which steps to follow?
+## 1. Create an account on Phantombuster.com 💻
+If you haven't already, create a **FREE** account on [Phantombuster](https://phantombuster.com/register). Our service will browse the web for you. It’s a website automator which runs in the cloud. Once done we'll follow up.
+
+
+## 2. Use this API on your account.👌
+Now that you're connected to Phantombuster, Click on the following button (it will open a new tab).
+
+<center><button type="button" class="btn btn-warning callToAction" onclick="useThisApi()">USE THIS API!</button></center>
+
+## 3. Click on Configure me!
+You'll now see the 3 configuration dots blinking. Click on them.
+
+<center>![](https://phantombuster.imgix.net/api-store/configure_me.JPG)</center>
+
+
+## 4. Instagram authentication 🔑 { argument }
+Because the script will manipulate Instagram for you, it needs to be logged on your Instagram account. For that you just need to copy paste your session cookie in the script argument:
+* Using Chrome, go to your Instagram homepage and open the inspector
+→ Right click anywhere on the page and select “Inspect” ![](https://phantombuster.imgix.net/api-store/Inspect+browser.png)
+→ <kbd>CMD</kbd>+<kbd>OPT</kbd>+<kbd>i</kbd> on macOS
+or
+→ <kbd>F12</kbd> or <kbd>CTRL</kbd>+<kbd>MAJ</kbd>+<kbd>i</kbd> on Windows
+
+* Locate the “Application” tab
+
+<center>![](https://phantombuster.imgix.net/api-store/li_at+1.png)</center>
+
+* Select “Cookies” > “http://www.instagram.com” on the left menu.
+
+<center>![](https://phantombuster.imgix.net/api-store/Instagram_Hashtag_Collector/sessionid_1.png)</center>
+
+* Locate the “`sessionid`” cookie.
+
+<center>![](https://phantombuster.imgix.net/api-store/Instagram_Hashtag_Collector/sessionid_2.png)</center/>
+
+* Copy what’s under “Value” (**Double click** on it then <kbd>Ctrl</kbd>+<kbd>C</kbd>) and paste it into your API _Configuration_
+
+_// How to access your cookies with <a href="https://developer.mozilla.org/en-US/docs/Tools/Storage_Inspector" target="_blank">Firefox</a> and <a href="https://www.macobserver.com/tmo/article/see_full_cookie_details_in_safari_5.1" target="_blank">Safari</a> //_
+
+## 5. Add a Google Spreadsheet 📑
+Below your session cookie you’ll find _Spreadsheet URL_
+
+Enter in the text field a link of a Google Spreadsheet with this same format _(only column A is mandatory)_:
+<center>![](https://phantombuster.imgix.net/api-store/Instagram_Profile_Scrapper/inst_prfl_scrrpr_spreadsheet.png)</center>
+
+Your spreadsheet should contain a list of Instagram Profile URLs (**one link per row**).
+You can specify the name of the column that contains the profile links. Simply enter the column name in the next text field.
+
+**Please make sure your file is publicly accessible!**
+
+You can also enter a CSV file URL, it will work the same :)
+You can also enter a single Instagram profile URL directly in the field.
+
+
+
+# Click on Launch & Enjoy!
+It’s done! All that is left to do is to click on "launch" to try your script!
+
+<center>![](https://phantombuster.imgix.net/api-store/launch.JPG)</center>
+
+# ⚙️️Repetition setup ⚙️ { repetition_setup }
+
+Now that your API is ready, you should set up repetitive launches. That way, your scraping will be spread over days, weeks or even months. You can also specify the number of profiles to scrape per launch, or leave that field blank to scrape every profile from your list.
+
+
+To do so, simply hit the “Settings” button to define when your API is launched:
+
+<center>![](https://phantombuster.imgix.net/api-store/settings-button.png)</center>
+
+Then, select a frequency:
+
+<center>![](https://phantombuster.imgix.net/api-store/repetition-setup.png)</center>
+
+Don't forget to click 💾 <span style="color:blue">Save</span> at the bottom of the page!
+
+For example, 10 profiles scraped per launch, 8 launches per day: you'll have a total of 80 profiles per day. We recommend not exceeding these values with this API.
+
+
+# Limits
+
+Please be aware that this API will manipulate your Instagram account on your behalf.
+
+Instagram has set rate limits of 200 requests an hour per token. We have noticed that after a few thousands profiles collected, you'll need to wait a few minutes before scraping again. We recommend spacing launches consequently.
