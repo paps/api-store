37a519e1fc9dcf7af13c583f70315de413ce181c
Nathan Gerszanowilsz <nathan.gerszanowilsz@phantombuster.com>
2018-08-21 18:00:33 +0200

    TFwerC: WIP adding statefulness #124

--- store/DEV/Twitter Follower Collector DEV/Twitter Follower Collector DEV.js
+++ store/DEV/Twitter Follower Collector DEV/Twitter Follower Collector DEV.js
@@ -24,77 +24,66 @@ const StoreUtilities = require("./lib-StoreUtilities")
 const utils = new StoreUtilities(nick, buster)
 const Twitter = require("./lib-Twitter")
 const twitter = new Twitter(nick, buster, utils)
-const MAX_FOLLOWERS_PER_ACCOUNT = -1
+// const { URL } = require("url")
+
 /* global $ */
 
 // }
 const gl = {}
-const rc = {}
 let graphqlUrl
-let requestSingleId
-let agentObject
-let interrupted
+// let agentObject
+// let interrupted
 let rateLimited
-let lastQuery
-let nextUrl
-let alreadyScraped
+// let lastQuery
+// let nextUrl
+// let alreadyScraped
+let twitterUrl
+
 
 const ajaxCall = (arg, cb) => {
-	try {
-		$.ajax({
-			url: arg.url,
-			type: "GET",
-			headers: arg.headers
-		})
-		.done(res => {
-			cb(null, res)
-		})
-		.fail(err => {
-			cb(err)
-		})
-	} catch (err) {
-		cb(err)
-	}
-}
-
-const ajax2 = (arg, cb) => {
 	cb(null, $.get(arg.url, arg.headers))
 }
-const getUrlsToScrape = (data, numberofProfilesperLaunch) => {
-	data = data.filter((item, pos) => data.indexOf(item) === pos)
-	const maxLength = data.length
-	if (maxLength === 0) {
-		utils.log("Input spreadsheet is empty OR we already scraped all the profiles from this spreadsheet.", "warning")
-		nick.exit()
-	}
-	return data.slice(0, Math.min(numberofProfilesperLaunch, maxLength)) // return the first elements
-}
+// const getUrlsToScrape = (data, numberofProfilesperLaunch) => {
+// 	data = data.filter((item, pos) => data.indexOf(item) === pos)
+// 	const maxLength = data.length
+// 	if (maxLength === 0) {
+// 		utils.log("Input spreadsheet is empty OR we already scraped all the profiles from this spreadsheet.", "warning")
+// 		nick.exit()
+// 	}
+// 	return data.slice(0, Math.min(numberofProfilesperLaunch, maxLength)) // return the first elements
+// }
 
 // Checks if a url is already in the csv
-const checkDb = (str, db) => {
-	for (const line of db) {
-		if (str === line.query && (line.query !== agentObject.lastQuery || line.error)) {
-			return false
-		}
-	}
-	return true
-}
+// const checkDb = (str, db) => {
+// 	for (const line of db) {
+// 		if (str === line.query && (line.query !== agentObject.lastQuery || line.error)) {
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
 // }
 const interceptTwitterApiCalls = e => {
 	if (e.response.url.indexOf("users?include_available") > -1 && e.response.status === 200) {
-		requestSingleId = e.requestId
+		// requestSingleId = e.requestId
 		graphqlUrl = e.response.url
 		console.log("interceptedi", e.response.url)
 
 		gl.headers = e.response.headers
 	}
+	if (e.response.url.indexOf("media_timeline") > -1 && e.response.status === 200) {
+		// console.log("interceptedURL", e.response.url)
+		// console.log("interceptedheaders", e.response.headers)
+
+		gl.headers = e.response.headers
+	}
 }
 
-const forgeNewUrl = (url, endCursor) => {
-	let urlObject = new URL(url)
-	urlObject.searchParams.set("max_position", endCursor)
-	return urlObject.href
-}
+// const forgeNewUrl = (url, endCursor) => {
+// 	let urlObject = new URL(url)
+// 	urlObject.searchParams.set("max_position", endCursor)
+// 	return urlObject.href
+// }
 
 const isUrl = str => url.parse(str).hostname !== null
 
@@ -102,10 +91,61 @@ const isTwitter = str => url.parse(str).hostname === "twitter.com"
 
 const removeNonPrintableChars = str => str.replace(/[^a-zA-Z0-9_@]+/g, "").trim()
 
+const scrapeFirstFollowers = async (tab, profileUrl) => {
+	await tab.waitUntilVisible("div.stream-container")
+	// await tab.screenshot(`scrapeF${new Date()}.png`)
+	// await buster.saveText(await tab.getContent(), `scrapeF${Date.now()}.html`)
+	let minPosition
+	try {
+		minPosition = await tab.evaluate((arg, cb) => cb(null, document.querySelector("div.stream-container").getAttribute("data-min-position")))
+		console.log("minPOS=", minPosition)
+	} catch (err) {
+		console.log("errpos", err)
+	}
+	let res
+	try {
+		const usl = `${profileUrl}/followers/users?min_position=${minPosition}`
+		console.log("uZZ", usl)
+		res = JSON.parse(await tab.evaluate(ajaxCall, {url: usl}))
+	} catch (err) {
+		// console.log("eoh", err)
+		console.log("Rate Limit Reached")
+		rateLimited = true
+		return []
+
+	}
+
+	return extractProfiles(res.items_html, profileUrl)
+}
+const scrapeFollowers = async (tab, profileUrl, twitterUrl, keepScraping) => {
+	// await tab.screenshot(`scrapeMain${new Date()}.png`)
+	// await buster.saveText(await tab.getContent(), `scrapeMain${Date.now()}.html`)
+	// const urlObject = new URL(graphqlUrl)
+	// const position = urlObject.searchParams.get("max_position")
+	let response
+	try {
+		// const usl = `https://twitter.com/${twitterHandle}/followers/users?min_position=${position}`
+		// console.log("uZZ", usl)
+		response = JSON.parse(await tab.evaluate(ajaxCall, {url: twitterUrl}))
+	} catch (err) {
+		// console.log("eoh", err)
+		console.log("Rate Limit Reached")
+
+		rateLimited = true
+		return [ [], twitterUrl, false ]
+	}
+	// console.log("rekmse", response)
+	const newPosition = response.min_position
+	keepScraping = response.has_more_items
+	twitterUrl = `${profileUrl}/followers/users?max_position=${newPosition}`
+	console.log("twitAR", twitterUrl)
+	return [ extractProfiles(response.items_html, profileUrl), twitterUrl, keepScraping ]
+}
+
 
 const getTwitterFollowers = async (tab, twitterHandle, followersPerAccount) => {
 	utils.log(`Getting followers for ${twitterHandle}`, "loading")
-	
+
 	// let twitterJson = await tab.driver.client.Network.getResponseBody({ requestId : requestSingleId })
 	// console.log("json", twitterJson)
 	// the regex should handle @xxx
@@ -114,37 +154,66 @@ const getTwitterFollowers = async (tab, twitterHandle, followersPerAccount) => {
 		// removing non printables characters from the extracted handle
 		twitterHandle = removeNonPrintableChars(twitterHandle)
 	}
+	const profileUrl = `https://twitter.com/${twitterHandle}`
+	console.log("url=", profileUrl)
+	await tab.open(profileUrl)
+	let result
 	try {
-		return await twitter.collectFollowers(tab, `https://twitter.com/${twitterHandle}/followers`, followersPerAccount)
+		result = await scrapeFirstFollowers(tab, profileUrl, followersPerAccount)
 	} catch (err) {
-		utils.log("Loaded 0 followers.", "done")
-		return []
+		console.log("ermm", err)
 	}
+	// console.log("on a les first")
+	if (!followersPerAccount || result.length < followersPerAccount) {
+		await tab.open(profileUrl + "/followers")
+		await tab.waitUntilVisible(".GridTimeline")
+		await tab.scrollToBottom()
+		await tab.wait(1000)
+		if (graphqlUrl) {
+			twitterUrl = graphqlUrl
+			let keepScraping = true
+			let res
+			do {
+				[ res, twitterUrl, keepScraping ] = await scrapeFollowers(tab, profileUrl, twitterUrl, followersPerAccount)
+				console.log("resLengh", res.length)
+				result = result.concat(res)
+				console.log("twitterURL", twitterUrl)
+				// console.log("resultencours: ", res)
+				console.log("totalngh", result.length)
+				if (followersPerAccount && result.length >= followersPerAccount) { 
+					console.log("on sort de la boucle")
+					break
+				}
+				if (rateLimited) {
+					console.log("on sort car Rate Limted")
+					break
+				}
+			} while (keepScraping)
+		}
+	}
+	if (followersPerAccount) { result = result.slice(0, followersPerAccount) }
+	return result
 }
 
-const extractProfiles = htmlContent => {
+const extractProfiles = (htmlContent, profileUrl) => {
 	let profileList = htmlContent.split("ProfileTimelineUser")
 	profileList.shift()
 	const result = []
-	console.log("pL", profileList)
+	// console.log("pL", profileList)
 	for (const profile of profileList) {
 		const data = {}
-		let pos = profile.indexOf("data-user-id=")
-		data.userId = profile.slice(pos + 14, pos + 14 + profile.slice(pos + 14).indexOf("\""))
-		pos = profile.indexOf("screen-name=")
-		data.screenName = profile.slice(pos + 13, pos + 13 + profile.slice(pos + 13).indexOf("\""))
-		pos = profile.indexOf("title=")
-		data.fullName = profile.slice(pos + 7, pos + 7 + profile.slice(pos + 7).indexOf("\""))
-		pos = profile.indexOf("src=")
-		data.imgUrl = profile.slice(pos + 5, pos + 5 + profile.slice(pos + 5).indexOf("\""))
-		pos = profile.indexOf("background-image: url(")
-		data.backgroundImg = profile.slice(pos + 22, pos + 22 + profile.slice(pos + 22).indexOf("\""))
-		pos = profile.indexOf("<p")
-		const pos2 = profile.indexOf("</p>")
-		const pContent = profile.slice(pos, pos2) + "</p>"
-		const chr = cheerio.load(pContent)
-		data.description = chr.text()
-
+		const chr = cheerio.load(profile)
+		data.userId = chr("div.ProfileCard").attr("data-user-id")
+		data.screeName = chr("div.ProfileCard").attr("data-screen-name")
+		data.fullName = chr("div.ProfileCard-avatarLink").attr("title")
+		data.imgUrl = chr("img.ProfileCard-avatarImage").attr("src")
+		const background = chr("a.ProfileCard-bg").attr("style")
+		const pos = background.indexOf("background-image")
+		if (pos > -1) {
+			data.backgroundImg = background.slice(pos + 22, background.indexOf(")"))
+		}
+		data.description = chr("p").text()
+		data.followerOf = profileUrl
 		result.push(data)
 	}
 	return result
@@ -165,7 +234,7 @@ const jsonToCsv = json => {
 	let {spreadsheetUrl, sessionCookie, followersPerAccount} = utils.validateArguments()
 
 	if (!followersPerAccount) {
-		followersPerAccount = MAX_FOLLOWERS_PER_ACCOUNT
+		followersPerAccount = 0
 	}
 
 	await twitter.login(tab, sessionCookie)
@@ -191,32 +260,18 @@ const jsonToCsv = json => {
 				break
 			}
 			const followers = await getTwitterFollowers(tab, twitterUrl, followersPerAccount)
+			result = result.concat(followers)
 			const newJson = {isFollowing: twitterUrl, followers}
 			const newCsv = jsonToCsv(newJson)
 			csvResult = csvResult.concat(newCsv)
 			jsonResult.push(newJson)
 		}
 	}
-	await tab.inject("../injectables/jquery-3.0.0.min.js")
-	let res
-	try {
-		console.log("url: ", graphqlUrl)
-		console.log("head", gl.headers)
-		// res = await tab.evaluate(ajaxCall, { url: graphqlUrl, headers: gl.headers })
-		res = JSON.parse(await tab.evaluate(ajax2, { url: graphqlUrl, headers: gl.headers }))
-	} catch (err) {
-		console.log("Errk", err)	
-	}
-	console.log("res", res)
-	console.log("reshtml", res.items_html)
-	console.log("minpos", res.min_position)
-	result = result.concat(extractProfiles(res.items_html))
 
-	// console.log("cheerio", chr.html())
+
 	tab.driver.client.removeListener("Network.responseReceived", interceptTwitterApiCalls)
 
-	// await utils.saveResults(jsonResult, csvResult, "result", ["profileUrl", "handle", "name", "bio", "isFollowing"])
-	await utils.saveResults(result, result, "result")
+	await utils.saveResults(result, result)
 	nick.exit()
 })()
 	.catch(err => {
