8ca0338688638a714e9cad565ea349c7fe4ee0f5
Martin Barreau <martin.barreau@gmail.com>
2018-11-08 17:15:34 +0100

    LGM API: removing voyager API calls, using DOM scraping

--- store/LinkedIn Group Members/LinkedIn Group Members.js
+++ store/LinkedIn Group Members/LinkedIn Group Members.js
@@ -20,140 +20,15 @@ const StoreUtilities = require("./lib-StoreUtilities")
 const utils = new StoreUtilities(nick, buster)
 const LinkedIn = require("./lib-LinkedIn")
 const linkedIn = new LinkedIn(nick, buster, utils)
-
-const { URL } = require("url")
-
-/* global $ */
-
-const LEGACY_ENDPOINT_PATTERN = 0
-const VOYAGER_ENDPOINT_PATTERN = 1
 // }
-const gl = {}
-
-let endpointPattern = LEGACY_ENDPOINT_PATTERN
-
-
-const extractQueryString = (url, field) => {
-	let urlRep
-
-	try {
-		urlRep = new URL(url)
-		return urlRep.searchParams.get(field)
-	} catch (err) {
-		return null
-	}
-}
-
-const updateUrlParam = (url, field, value) => {
-	let urlRep
-
-	try {
-		urlRep = new URL(url)
-		urlRep.searchParams.set(field, value.toString())
-		return urlRep.toString()
-	} catch (err) {
-		return url
-	}
-}
 
 /**
- * @param {Object} profile - raw voyager AJAX call response
- * @return {{ skillX: String, region: string, industry: string, school: string, groupX: String, groupUrlX: String }|null} - profile data needed for LGM script otherwise null
+ * @async
+ * @description Check if the page is a valid group (kill the API if groupUrl doesn't represent an LinkedIn group)
+ * @param {Object} tab
+ * @param {String} groupUrl
+ * @return {Promise<Boolean>}
  */
- const extractProfileViewData = res => {
-	let ret = {}
-
-	if (res.profile) {
-		if (res.profile.locationName) {
-			ret.region = res.profile.locationName
-		}
-		if (res.profile.industryName) {
-			ret.industry = res.profile.industryName
-		}
-		if (res.skillView) {
-			if (Array.isArray(res.skillView.elements) && res.skillView.elements.length > 0) {
-				let i = 0
-				for (const skill of res.skillView.elements) {
-					// Top skills returned
-					if (i < 3) {
-						ret[`skill${i + 1}`] = skill.name
-					}
-					i++
-				}
-			}
-		}
-		if (res.educationView) {
-			if (Array.isArray(res.educationView.elements) && res.educationView.elements.length > 0) {
-				ret["school"] = res.educationView.elements[0].schoolName;
-			}
-		}
-	} else {
-		ret = null
-	}
-
-	return ret
-}
-
-/**
- * @description Browser context function used to return if possible skills, groups & localization of a group member
- * The function only returns 3 groups & skills see #94 for more informations (scraping data which are visible from a mouse hover)
- * @param {*} arg - Browser context arguments (LinkedIn member ID, and intercepted headers to make the AJAX calls)
- * @param {*} cb - Callback used to exit the Browser context
- */
-const getMemberDetails = (arg, cb) => {
-	try {
-		$.ajax({
-			url: `https://www.linkedin.com/communities-api/v1/profile/${arg.id}`,
-			type: "GET",
-			headers: arg.headers
-		})
-		.done(res => {
-			const member = {}
-			let i = 0
-			if (Array.isArray(res.data)) {
-				if (res.data[0].skills) {
-					for (const one of res.data[0].skills) {
-						if (i < 3) {
-							member[`skill${i + 1}`] = one.localizedName
-						}
-						i++
-					}
-				}
-				if (res.data[0].region) {
-					member.location = res.data[0].region
-				}
-
-				if (res.data[0].industry) {
-					member.industry = res.data[0].industry
-				}
-
-				if (res.data[0].education) {
-					member["school"] = res.data[0].education.localizedSchoolName
-				}
-
-				i = 0
-				if (res.data[0].memberGroups) {
-					for (const group of res.data[0].memberGroups) {
-						if (i < 3) {
-							member[`group${i + 1}`] = group.name
-							member[`groupUrl${i + 1}`] = `https://www.linkedin.com/groups/${group.id}`
-						}
-						i++
-					}
-				}
-			}
-
-			cb(null, member)
-		})
-		.fail(err => {
-			cb(err.toString())
-		})
-	} catch (err) {
-		cb(err)
-	}
-}
-
-// Check if the page is a valid group
 const checkGroup = async (tab, groupUrl) => {
 	groupUrl = groupUrl.replace(/\/$/, "")
 	await tab.open(groupUrl + "/members")
@@ -184,209 +59,135 @@ const checkGroup = async (tab, groupUrl) => {
 }
 
 /**
- * @description Loop over the pages of the group to get all members (maximum is 2500 => LinkedIn restriction)
- * @param {Object} tab - Nickjs Tab Object
- * @param {Number} patternNumber - URL pattern used to make AJAX call (0 => legacy / 1 => new endpoint)
- * @return {Promise<Array<Object>>} all members
+ * @description Scrape all members present in the DOM
+ * @param {{ sel: String }} arg - CSS selector representing a group member
+ * @param {*} cb
+ * @return {Promise<Array<Object>>} members
  */
-const getGroupMembers = async (tab, patternNumber) => {
-	let errors = 0
+const scrapeMembers = (arg, cb) => {
+	const scrapeResult = el => {
+		const res = {}
+		const profileSelector = el.querySelector("artdeco-entity-lockup-image")
+		const headlineSelector = el.querySelector("artdeco-entity-lockup-content")
+		if (profileSelector) {
+			let name = null
+			res.profileUrl = profileSelector.querySelector("a[data-control-name=\"view_profile\"]") ? profileSelector.querySelector("a[data-control-name=\"view_profile\"]").href : null
+			res.fullName = profileSelector.querySelector("div.presence-entity__image") ? profileSelector.querySelector("div.presence-entity__image").getAttribute("aria-label") : null
+			name = res.fullName ? res.fullName.split(" ") : null
+			res.lastName = name ? name.pop() : null
+			res.firstName = name ? name.shift() : null
+		}
+
+		if (headlineSelector) {
+			res.headline = headlineSelector.querySelector("artdeco-entity-lockup-subtitle") ? headlineSelector.querySelector("artdeco-entity-lockup-subtitle").textContent.trim() : null
+		}
+		res.timestamp = (new Date()).toISOString()
+		return res
+	}
+	cb(null, Array.from(document.querySelectorAll(arg.sel)).map(el => scrapeResult(el)))
+}
+
+/**
+ * @description Delete X DOM elements
+ * @param {{ sel: String, count: Number }} arg- CSS selector representing a group member
+ * @param {*} cb
+ * @return {Promise<null>}
+ */
+const removeElements = (arg, cb) => {
+	const elements = document.querySelectorAll(arg.sel)
+
+	for (let i = 0, len = arg.count || 20; i < len; i++) {
+		elements[i].parentNode.removeChild(elements[i])
+	}
+	cb(null)
+}
+
+/**
+ * @description Return group members count
+ * @param {{ sel: String }} arg
+ * @param {*} cb
+ * @return {Promise<Number>}
+ */
+const getMembersCount = (arg, cb) => {
+	let count = document.querySelector("artdeco-typeahead h1") || 0
+	if (!count) cb(null, count)
+	count = parseInt(count.textContent.trim().replace(/\s/g, "").match(/([\d,. ]+)/).find(el => !isNaN(parseInt(el, 10))), 10)
+	cb(null, count)
+}
+
+/**
+ * @description Return how many members are loaded in the DOM
+ * @param {{ sel: String }} arg
+ * @param {*} cb
+ * @return {Promise<Number>}
+ */
+const getDomElementsCount = (arg, cb) => cb(null, document.querySelectorAll(arg.sel).length)
+
+/**
+ * @async
+ * @param {Object} tab
+ * @return {Promise<Array<Object>>} scraped members
+ */
+const getMembers = async tab => {
 	const members = []
-	while (true) {
+	const sel = "artdeco-typeahead-results-list.groups-members-list__results-list artdeco-typeahead-result"
+	let lastCount = 0
+	// Wait until the result list is visible
+	await tab.waitUntilVisible("artdeco-typeahead-results-list.groups-members-list__results-list", 15000)
+	const count = await tab.evaluate(getMembersCount)
+	utils.log(`Scraping ${count} members`, "info")
+	while (members.length + 1 < count) {
 		const timeLeft = await utils.checkTimeLeft()
 		if (!timeLeft.timeLeft) {
 			utils.log(`Stopped getting group members: ${timeLeft.message}`, "warning")
 			break
 		}
-		let response
+
+		// Get members profile
+		const res = await tab.evaluate(scrapeMembers, { sel })
+		// Removing duplicated scraped elements
+		members.push(...utils.filterRightOuter(members, res))
+		if ((members.length - lastCount) >= 100) {
+			utils.log(`Got ${members.length + 1} members from the list.`, "info")
+			lastCount = members.length
+		}
+		const elementsCount = await tab.evaluate(getDomElementsCount, { sel })
+
+		// Clean the DOM if to many elements are loaded
+		if (elementsCount >= 50) {
+			await tab.evaluate(removeElements, { sel, count: 40 })
+		}
+		// Load new members...
 		try {
-			response = await tab.evaluate(ajaxCall, {url: gl.url, headers: gl.headers})
-		} catch (e) {
-			++errors
-			if (errors >= 10) {
-				utils.log(`${errors} errors, stopping`, "warning")
-				break
-			}
-			utils.log(`(Hmm, LinkedIn did not respond correctly, retrying...) [${errors}/10]`, "info")
-			await tab.wait(3000 + 4000 * errors)
-			if (patternNumber === VOYAGER_ENDPOINT_PATTERN) {
-				let lastIndex = extractQueryString(gl.url, "start")
-				let lastCount = extractQueryString(gl.url, "count")
-				lastIndex = lastIndex ? parseInt(lastIndex, 10) : null
-				lastCount = lastCount ? parseInt(lastCount, 10) : null
-				lastIndex += (lastCount + 1)
-				/**
-				 * Max value is 100
-				 * This AJAX call is limited 20 people, in order to not randomly fails
-				 */
-				lastCount = 20
-				gl.url = updateUrlParam(gl.url, "start", lastIndex)
-				gl.url = updateUrlParam(gl.url, "count", lastCount)
-				gl.url = decodeURIComponent(gl.url)
-			}
-				continue
-
+			await tab.scrollToBottom()
+			await tab.waitUntilVisible("div.artdeco-spinner", 30000)
+			await tab.waitWhileVisible("div.artdeco-spinner", 30000)
+		} catch (err) {
+			break
 		}
-		errors = 0
-		if (response.data) {
-			if (patternNumber === VOYAGER_ENDPOINT_PATTERN) {
-				response.data = response.included.filter(el => el["$type"] && el["$type"] === "com.linkedin.voyager.identity.shared.MiniProfile")
-			}
-			for (const item of response.data) {
-				let newMember = {}
-				if (item.mini) {
-					const mini = item.mini
-
-					// Enhancement #94
-					if (mini.links && mini.links.nonIterableMembershipLink) {
-						try {
-							const details = await tab.evaluate(getMemberDetails, { id: mini.links.nonIterableMembershipLink.split("/").pop(), headers: gl.headers })
-							for (const one of Object.keys(details)) {
-								newMember[one] = details[one]
-							}
-						} catch (err) { /* No error handler needed, this isn't a fatal error */ }
-					}
-
-					newMember.profileUrl = mini.profileUrl
-					newMember.firstName = mini.firstName
-					newMember.lastName = mini.lastName
-					newMember.fullName = mini.firstName + " " + mini.lastName
-					newMember.headline = mini.headline
-				} else {
-					newMember.profileUrl = `https://www.linkedin.com/in/${item.publicIdentifier}`
-					newMember.firstName = item.firstName
-					newMember.lastName = item.lastName
-					newMember.fullName = `${item.firstName} ${item.lastName}`
-					newMember.headline = item.occupation
-					/**
-					 * publicIdentifier is mandatory to get all LPS API data in LGM
-					 */
-					let headers = Object.assign({}, gl.headers)
-					headers["Accept"] = "application/json"
-					let moreInfos = null
-					try {
-						moreInfos = await tab.evaluate(ajaxCall, { url: `https://www.linkedin.com/voyager/api/identity/profiles/${item.publicIdentifier}/profileView`, headers })
-						moreInfos = extractProfileViewData(moreInfos)
-						newMember = Object.assign(newMember, moreInfos)
-					} catch (err) {
-						/* ... */
-					}
-				}
-				if (item.currentPosition) {
-					const currentPosition = item.currentPosition
-					newMember.company = currentPosition.companyName
-					newMember.title = currentPosition.title
-					if (currentPosition.companyId) {
-						newMember.companyUrl = "https://www.linkedin.com/company/" + currentPosition.companyId
-					}
-				}
-				newMember.timestamp = (new Date()).toISOString()
-				members.push(newMember)
-			}
-		}
-
-		if (patternNumber === LEGACY_ENDPOINT_PATTERN) {
-			if (response.meta && response.meta.next && response.data.length > 0) {
-				utils.log(`Got ${members.length} members from the list.`, "info")
-				gl.url = response.meta.next
-			} else {
-					utils.log(`Got ${members.length} members from the list.`, "done")
-					break
-			}
-		} else if (patternNumber === VOYAGER_ENDPOINT_PATTERN) {
-			let lastIndex = extractQueryString(gl.url, "start")
-			let lastCount = extractQueryString(gl.url, "count")
-			lastIndex = lastIndex ? parseInt(lastIndex, 10) : null
-			lastCount = lastCount ? parseInt(lastCount, 10) : null
-			lastIndex += (lastCount + 1)
-			/**
-			 * Max value is 100
-			 * This AJAX call is limited 20 people, in order to not randomly fails
-			 */
-			lastCount = 20
-			// if (lastIndex > 200) {
-			// 	lastIndex = 500
-			// }
-			gl.url = updateUrlParam(gl.url, "start", lastIndex)
-			gl.url = updateUrlParam(gl.url, "count", lastCount)
-			gl.url = decodeURIComponent(gl.url)
-			if (response.data.length < 1) {
-				utils.log(`Got ${members.length} members from the list`, "done")
-				break
-			} else {
-				utils.log(`Got ${members.length} members from the list`, "info")
-			}
-		}
-
 	}
+	utils.log(`${members.length + 1} members scraped`, "done")
 	return members
-}
-
-// HTTP request via ajax
-const ajaxCall = (arg, callback) => {
-	try {
-		$.ajax({
-			url: arg.url,
-			type: "GET",
-			headers: arg.headers
-		})
-		.done(data => {
-			callback(null, data)
-		})
-		.fail(err => {
-			callback(err)
-		})
-	} catch (e) {
-		callback(e.toString())
-	}
-}
-
-// Get http request headers
-const onHttpRequest = (e) => {
-	const firstPattern = "https://www.linkedin.com/communities-api/v1/memberships/community"
-	const secondPattern = "https://www.linkedin.com/voyager/api/groups/groups/urn:li:group:"
-	if (e.request.url.indexOf(firstPattern) > -1 || e.request.url.indexOf(secondPattern) > -1) {
-		gl.headers = e.request.headers
-		gl.url = e.request.url
-		if (e.request.url.indexOf(firstPattern) > -1) {
-			endpointPattern = LEGACY_ENDPOINT_PATTERN
-		} else if (e.request.url.indexOf(secondPattern) > -1) {
-			endpointPattern = VOYAGER_ENDPOINT_PATTERN
-		}
-	}
 }
 
 // Main function to launch everything and handle errors
 ;(async () => {
 	const tab = await nick.newTab()
-	const [ sessionCookie, groupUrl, groupName ] = utils.checkArguments([
-		{ name: "sessionCookie", type: "string", length: 10 },
-		{ name: "groupUrl", type: "string", length: 10 },
-		{ name: "groupName", type: "string", default: "result" },
-	])
-	tab.driver.client.on("Network.requestWillBeSent", onHttpRequest)
+	let { sessionCookie, groupUrl, groupName } = utils.validateArguments()
+	const members = []
+
+	if (!groupName) {
+		groupName = "result"
+	}
 	await linkedIn.login(tab, sessionCookie)
 	await checkGroup(tab, groupUrl)
-	await tab.wait(3000)
-	if (!gl.url) {
-		if (await tab.isPresent("a.pagination-link.next:not(.hidden)")) {
-			await tab.click("a.pagination-link.next:not(.hidden)")
-		} else {
-			await tab.scrollToBottom()
-		}
-		await tab.wait(3000)
-		if (!gl.url) {
-			throw ("Could not get members of this group.")
-		}
+	try {
+		utils.log(`Getting members for group ${groupName}...`, "loading")
+		const res = await getMembers(tab)
+		members.push(...res)
+	} catch (err) {
+		utils.log(`Error while scraping: ${err.message || err}`, "warning")
 	}
-	tab.driver.client.removeListener("Network.requestWillBeSent", onHttpRequest)
-	if (endpointPattern === VOYAGER_ENDPOINT_PATTERN) {
-		await tab.scroll(0, 0)
-		gl.url = updateUrlParam(gl.url, "start", 0)
-		gl.url = decodeURIComponent(updateUrlParam(gl.url, "count", 20))
-	}
-	utils.log(`Getting members for group ${groupName}...`, "loading")
-	const members = await getGroupMembers(tab, endpointPattern)
 	await utils.saveResults(members, members, groupName)
 	await linkedIn.saveCookie()
 	nick.exit()
