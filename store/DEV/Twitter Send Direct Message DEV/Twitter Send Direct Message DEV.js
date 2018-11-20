47c8b3fb30dfcdb05709773f683d04af3d614399
Martin Barreau <martin.barreau@gmail.com>
2018-11-20 18:05:42 +0100

    TSDM API: WIP can send message, need more check when writting the destination handle

--- store/DEV/Twitter Send Direct Message DEV/Twitter Send Direct Message DEV.js
+++ store/DEV/Twitter Send Direct Message DEV/Twitter Send Direct Message DEV.js
@@ -1,6 +1,6 @@
 // Phantombuster configuration {
 "phantombuster command: nodejs"
-"phantombuster package: 4"
+"phantombuster package: 4" // TODO: upgrade to package 5 for production (package 4 is better when debugging)
 "phantombuster dependencies: lib-StoreUtilities.js, lib-Twitter.js"
 "phantombuster flags: save-folder"
 
@@ -36,7 +36,8 @@ const SELECTORS = {
 	msgDestinationSelector: "textarea.twttr-directmessage-input",
 	initConvSelector: "button.dm-initiate-conversation",
 	textEditSelector: "div.DMComposer-editor.tweet-box",
-	sendSelector: "button.tweet-action"
+	sendSelector: "button.tweet-action",
+	closeSelector: "button.DMActivity-close"
 }
 
 /* global $ */
@@ -62,13 +63,13 @@ const isTwitterUrl = url => {
 /**
  * @param {{ sel: String }} arg
  * @param {Function} cb
- * @throws after 30s if the selector still disabled
+ * @throws String after 30s if the selector still disabled
  */
 const waitWhileDisabled = (arg, cb) => {
 	const startTime = Date.now()
 	const idle = () => {
 		const sel = document.querySelector(arg.sel)
-		if (sel && sel.disabled) {
+		if (sel && sel.classList.contains("disabled")) {
 			if ((Date.now() - startTime) >= 30000) {
 				return cb(`${arg.sel} still disabled after 30s of idle`)
 			}
@@ -83,13 +84,13 @@ const waitWhileDisabled = (arg, cb) => {
 /**
  * @param {{ sel: String }} arg
  * @param {Function} cb
- * @throws after 30s if the selector still enabled
+ * @throws String after 30s if the selector still enabled
  */
 const waitWhileEnabled = (arg, cb) => {
 	const startTime = Date.now()
 	const idle = () => {
 		const sel = document.querySelector(arg.sel)
-		if (sel && !sel.disabled) {
+		if (sel && !sel.classList.contains("disabled")) {
 			if ((Date.now() - startTime) >= 30000) {
 				return cb(`${arg.sel} still enabled after 30s of idle`)
 			}
@@ -113,7 +114,7 @@ const _sendMessage = (arg, cb) => {
  * @async
  * @param {Object} tab - Nickjs tab instance
  * @param {Boolean} [openNewMsg]
- * @throws on loading failures
+ * @throws String on loading failures
  * @return {Promise<Boolean>}
  */
 const openMessagesPage = async (tab, loadMsgComposer = false) => {
@@ -130,20 +131,34 @@ const openMessagesPage = async (tab, loadMsgComposer = false) => {
 	return true
 }
 
+/**
+ * TODO: check if the handle is correctly written
+ */
 const startConversation = async (tab, handle) => {
 	await tab.evaluate((arg, cb) => cb(null, document.querySelector(arg.sel).value = arg.handle), {sel: SELECTORS.msgDestinationSelector, handle })
 	await tab.screenshot(`composer-${Date.now()}.jpg`)
 	await tab.click(SELECTORS.initConvSelector)
-	await tab.evaluate(waitWhileDisabled, { sel: SELECTORS.initConvSelector })
+	try {
+		await tab.evaluate(waitWhileEnabled, { sel: SELECTORS.initConvSelector })
+	} catch (err) {
+		utils.log(`startConversation: ${err.message || err}`, "error")
+	}
 	await tab.waitUntilVisible([ SELECTORS.textEditSelector, SELECTORS.sendSelector ], 15000, "and")
 }
 
 const sendMessage = async (tab, message) => {
-	let transformedMsg = message.replace("\n", "\r\n")
-	await tab.evaluate((arg, cb) => cb(null, document.querySelector(arg.sel).textContent = arg.message), { sel: SELECTORS.textEditSelector, message: transformedMsg })
-	await tab.sendKeys(SELECTORS.textEditSelector, transformedMsg, { reset: false, keepFocus: true })
-	await tab.evaluate(_sendMessage, { sel: SELECTORS.textEditSelector })
-	await tab.evaluate(waitWhileEnabled, { sel: SELECTORS.sendSelector })
+	//let chuncks = message.split("\n").filter(el => el)
+	try {
+		await tab.evaluate(waitWhileEnabled, { sel: SELECTORS.sendSelector })
+		await tab.sendKeys(SELECTORS.textEditSelector, message, { reset: true, keepFocus: true })
+		await tab.screenshot(`sendKey-${Date.now()}.jpg`)
+		await tab.evaluate(_sendMessage, { sel: SELECTORS.textEditSelector })
+		await tab.screenshot(`sendedMsg-${Date.now()}.jpg`)
+	} catch (err) {
+		await tab.screenshot(`send-error${Date.now()}.jpg`)
+		utils.log(`sendMessage: ${err.message || err}`, "error")
+	}
+	await tab.click(SELECTORS.closeSelector)
 }
 
 
@@ -162,18 +177,23 @@ const sendMessage = async (tab, message) => {
 
 	db = await utils.getDb(csvName + ".csv")
 
-	if (spreadsheetUrl) {
-		if (isTwitterUrl(spreadsheetUrl)) {
-			queries = [ spreadsheetUrl ]
-		} else if (isUrl(spreadsheetUrl)) {
-			queries = await utils.getDataFromCsv(spreadsheetUrl, columnName)
-		} else {
-			queries = [ spreadsheetUrl ]
-		}
+	if (spreadsheetUrl && isUrl(spreadsheetUrl)) {
+		queries = isTwitterUrl(spreadsheetUrl) ? [ spreadsheetUrl ] : await utils.getDataFromCsv(spreadsheetUrl, columnName)
+	} else {
+		queries = [ spreadsheetUrl ]
 	}
 
 	if (typeof queries === "string") {
 		queries = [ queries ]
+	}
+
+	if (typeof numberOfProfilesPerLaunch === "number") {
+		queries = queries.slice(0, numberOfProfilesPerLaunch)
+	}
+
+	if (queries.length < 1) {
+		utils.log("Input is empty", "warning")
+		nick.exit()
 	}
 
 	await twitter.login(tab, sessionCookie)
@@ -184,14 +204,15 @@ const sendMessage = async (tab, message) => {
 			await openMessagesPage(tab, true)
 			await startConversation(tab, profile.handle)
 			await sendMessage(tab, message)
-			await tab.screenshot(`init-${Date.now()}.jpg`)
+			// TODO: wait until a new message appears in the thread
+			await tab.wait(15000)
 			console.log(JSON.stringify(profile, null, 4))
 		} catch (err) {
 			console.log(err.message || err)
-			await tab.screenshot(`err-${Date.now()}.jpg`)
+			console.log(err.stack || "no stack")
 		}
 	}
-	// await utils.saveResults([], null, null, false)
+	await utils.saveResults(db, db, csvName, false)
 	nick.exit()
 })()
 .catch(err => {
@@ -199,3 +220,4 @@ const sendMessage = async (tab, message) => {
 	console.log(err.stack || "no stack")
 	nick.exit(1)
 })
+
