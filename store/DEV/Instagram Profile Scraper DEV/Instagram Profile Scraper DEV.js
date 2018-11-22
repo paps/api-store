93d090d55459ef74f7ffc276f5d6b376012afa81
Nathan Gerszanowilsz <fffan@free.fr>
2018-11-22 18:56:54 +0100

    IPS: enhanced JSON scraping dev

--- store/DEV/Instagram Profile Scraper DEV/Instagram Profile Scraper DEV.js
+++ store/DEV/Instagram Profile Scraper DEV/Instagram Profile Scraper DEV.js
@@ -1,7 +1,7 @@
 // Phantombuster configuration {
 "phantombuster command: nodejs"
 "phantombuster package: 5"
-"phantombuster dependencies: lib-StoreUtilities.js, lib-Instagram-DEV.js"
+"phantombuster dependencies: lib-StoreUtilities.js, lib-Instagram.js"
 
 const Buster = require("phantombuster")
 const buster = new Buster()
@@ -18,7 +18,7 @@ const nick = new Nick({
 
 const StoreUtilities = require("./lib-StoreUtilities")
 const utils = new StoreUtilities(nick, buster)
-const Instagram = require("./lib-Instagram-DEV")
+const Instagram = require("./lib-Instagram")
 const instagram = new Instagram(nick, buster, utils)
 // }
 
@@ -93,6 +93,81 @@ const scrapePage = (arg, callback) => {
 	callback(null, data)
 }
 
+const scrapeData = async (tab, query, profileUrl) => {
+	const jsonUrl = `${profileUrl}?__a=1`
+	await tab.open(jsonUrl)
+	let instagramJsonCode = await tab.getContent()
+	const partCode = instagramJsonCode.slice(instagramJsonCode.indexOf("{"))
+	instagramJsonCode = JSON.parse(partCode.slice(0, partCode.indexOf("<")))
+	const data = instagramJsonCode.graphql.user
+	const scrapedData = { query, profileUrl }
+	scrapedData.bio = data.biography
+	if (data.blocked_by_viewer) {
+		scrapedData.status = "Blocked"
+	}
+	scrapedData.followersCount = data.edge_followed_by.count
+	scrapedData.followingCount = data.edge_follow.count
+	if (data.followed_by_viewer) {
+		scrapedData.status = "Following"
+	}
+	if (data.follows_viewer) {
+		scrapedData.followsViewer = "Follows you"
+	}
+	scrapedData.fullName = data.full_name
+	scrapedData.instagramID = data.id
+	if (data.is_business_account) {
+		scrapedData.businessAccount = "Business Account"
+	}
+	if (data.is_joined_recently) {
+		scrapedData.joinedRecently = "Joined Recently"
+	}
+	if (data.business_category_name) {
+		scrapedData.businessCategory = data.business_category_name
+	}
+	if (data.business_email) {
+		scrapedData.businessCategoryName = data.business_email
+	}
+	if (data.business_phone_number) {
+		scrapedData.Phone = data.phone_number
+	}
+	if (data.business_address_json) {
+		const businessAddress = JSON.parse(data.business_address_json)
+		if (businessAddress.street_address) {
+			scrapedData.businessStreetAddress = businessAddress.street_address
+		}
+		if (businessAddress.zip_code) {
+			scrapedData.businessZipCode = businessAddress.zip_code
+		}
+		if (businessAddress.city_name) {
+			scrapedData.businessCity = businessAddress.city_name
+		}
+		if (businessAddress.region_name) {
+			scrapedData.businessRegion = businessAddress.region_name
+		}
+		if (businessAddress.country_code) {
+			scrapedData.businessCountryCode = businessAddress.country_code
+		}
+	}
+	if (data.is_private) {
+		scrapedData.private = "Private"
+	}
+	if (data.is_verified) {
+		scrapedData.verified = "Verified"
+	}
+	scrapedData.mutualFollowersCount = data.edge_mutual_followed_by.count
+	scrapedData.imageUrl = data.profile_pic_url_hd
+	if (data.requested_by_viewer) {
+		scrapedData.requestedByViewer = "Requested"
+	}
+	scrapedData.postsCount = data.edge_owner_to_timeline_media.count
+	scrapedData.profileName = data.username
+	if (data.external_url) {
+		scrapedData.website = data.external_url
+	}
+	scrapedData.timestamp = (new Date()).toISOString()
+	return scrapedData
+}
+
 // Main function that execute all the steps to launch the scrape and handle errors
 ;(async () => {
 	let { sessionCookie, spreadsheetUrl, columnName, numberOfProfilesPerLaunch , csvName } = utils.validateArguments()
@@ -122,6 +197,7 @@ const scrapePage = (arg, callback) => {
 
 	console.log(`URLs to scrape: ${JSON.stringify(urls, null, 4)}`)
 	const tab = await nick.newTab()
+	const jsonTab = await nick.newTab()
 	await instagram.login(tab, sessionCookie)
 
 	let pageCount = 0
@@ -144,14 +220,16 @@ const scrapePage = (arg, callback) => {
 			}
 			const profileUrl = await tab.getUrl()
 			result = result.concat(await tab.evaluate(scrapePage, { url, profileUrl }))
-
+			console.log("r1", result)
+			result = result.concat(await scrapeData(jsonTab, url, profileUrl))
+			console.log("r2", result)
 		} catch (err) {
 			utils.log(`Can't scrape the profile at ${url} due to: ${err.message || err}`, "warning")
 			continue
 		}
 	}
 
-	await utils.saveResults(result, result, csvName, null, false)
+	await utils.saveResults(result, result, csvName)
 	nick.exit(0)
 })()
 .catch(err => {
