fa71f6539f4909713762a0ebc86e2bf643db9bc7
Nathan Gerszanowilsz <fffan@free.fr>
2018-12-13 15:31:43 +0100

    LCE: updated version

--- store/DEV/LinkedIn Companies Employees DEV/LinkedIn Companies Employees DEV.js
+++ store/DEV/LinkedIn Companies Employees DEV/LinkedIn Companies Employees DEV.js
@@ -1,6 +1,6 @@
 // Phantombuster configuration {
 "phantombuster command: nodejs"
-"phantombuster package: 4"
+"phantombuster package: 5"
 "phantombuster dependencies: lib-StoreUtilities.js, lib-LinkedIn.js"
 
 const { URL } = require("url")
@@ -23,20 +23,8 @@ const StoreUtilities = require("./lib-StoreUtilities")
 const utils = new StoreUtilities(nick, buster)
 const LinkedIn = require("./lib-LinkedIn")
 const linkedIn = new LinkedIn(nick, buster, utils)
-const DB_NAME = "result.csv"
 // }
 
-const jsonToCsv = json => {
-	const csv = []
-	for (const company of json) {
-		for (const employee of company.employees) {
-			const newEmployee = Object.assign({}, employee)
-			newEmployee.companyUrl = company.url
-			csv.push(newEmployee)
-		}
-	}
-	return csv
-}
 
 const isLinkedUrl = target => {
 	try {
@@ -51,20 +39,20 @@ const scrapeCompanyLink = (arg, callback) => {
 	callback(null, document.querySelector("li.search-result a.search-result__result-link") ? document.querySelector("li.search-result a.search-result__result-link").href : null)
 }
 
-const scrapeResults = (args, callback) => {
+const scrapeResults = (arg, cb) => {
 	const results = document.querySelectorAll("ul.results-list > li, ul.search-results__list > li")
-	const linkedInUrls = []
+	const scrapedData = []
 	for (const result of results) {
 		if (result.querySelector(".search-result__result-link")) {
-			const scrapedEmployee = {}
-			const url = result.querySelector(".search-result__result-link").href
+			const scrapedEmployee = { query: arg.query, timestamp: (new Date()).toISOString() }
+			const profileUrl = result.querySelector(".search-result__result-link").href
 			let currentJob = "none"
 			if (result.querySelector("p.search-result__snippets")) {
 				currentJob = result.querySelector("p.search-result__snippets").textContent.trim()
 				currentJob = currentJob.replace(/^.+ ?: ?\n/, "").trim()
 				scrapedEmployee.currentJob = currentJob
 			}
-			if (url !== window.location.href + "#") {
+			if (profileUrl !== window.location.href + "#") {
 				let name
 				if (result.querySelector("figure.search-result__image > img")) {
 					name = result.querySelector("figure.search-result__image > img").alt
@@ -74,26 +62,21 @@ const scrapeResults = (args, callback) => {
 					name = result.querySelector(".actor-name").textContent
 				}
 				scrapedEmployee.name = name
-				scrapedEmployee.url = url
+				scrapedEmployee.profileUrl = profileUrl
 			}
 			scrapedEmployee.location = (result.querySelector("div.search-result__info > p.subline-level-2")) ? result.querySelector("div.search-result__info > p.subline-level-2").textContent.trim() : "No location found"
 			scrapedEmployee.job = result.querySelector("div.search-result__info > p.subline-level-1") ? result.querySelector("div.search-result__info > p.subline-level-1").textContent.trim() : "no job found"
-			linkedInUrls.push(scrapedEmployee)
+			scrapedData.push(scrapedEmployee)
 		}
 	}
-	callback(null, linkedInUrls)
+	cb(null, scrapedData)
 }
 
-const getEmployees = async (tab, id, numberOfPage, waitTime) => {
+const getEmployees = async (tab, id, query, numberOfPage) => {
 	utils.log(`Getting employees for company with id: ${id}...`, "loading")
-	let result = {
-		employees: []
-	}
+	let result = []
 	const selectors = ["div.search-no-results__container", "div.search-results-container"]
 	for (let i = 1; i <= numberOfPage; i++) {
-		if (waitTime) {
-			await tab.wait(waitTime)
-		}
 		utils.log(`Getting urls from page ${i}...`, "loading")
 		await tab.open(`https://www.linkedin.com/search/results/people/?facetCurrentCompany=["${id}"]&page=${i}`)
 		const selector = await tab.waitUntilVisible(selectors, 5000, "or")
@@ -106,20 +89,17 @@ const getEmployees = async (tab, id, numberOfPage, waitTime) => {
 			}
 			await tab.scrollToBottom()
 			await tab.wait(1500)
-			result.employees = result.employees.concat(await tab.evaluate(scrapeResults))
-			result.employees = result.employees.map(el => {
-				el.timestamp = (new Date()).toISOString()
-				return el
-			})
+			result = result.concat(await tab.evaluate(scrapeResults, { query }))
 			let hasReachedLimit = await linkedIn.hasReachedCommercialLimit(tab)
 			if (hasReachedLimit) {
 				utils.log(hasReachedLimit, "info")
 				break
 			}
 			utils.log(`Got employees for page ${i}`, "done")
+			await tab.wait(1500 + 1000 * Math.random())
 		}
 	}
-	utils.log(`All pages with employees scrapped for company with id: ${id}`, "done")
+	utils.log(`All pages with employees scrapped for company: ${query}`, "done")
 	return result
 }
 
@@ -161,7 +141,7 @@ const getIdFromUrl = async (url, tab) => {
 			await tab.waitUntilVisible("div.search-results-container")
 			url = await tab.evaluate(scrapeCompanyLink)
 			if (!url) {
-				throw "No company found."
+				throw "No company found"
 			} else {
 				const id = new URL(url).pathname.slice(9).replace(/\D+/g, "")
 				return id
@@ -186,7 +166,7 @@ const getIdFromUrl = async (url, tab) => {
 			try {
 				await tab.untilVisible(".org-company-employees-snackbar__details-highlight")
 			} catch (err) {
-				throw "no employees found from the LinkedIn company page"
+				throw "No employees found"
 			}
 			let tmp = await tab.evaluate((argv, cb) => {
 				let ids = document.querySelector(".org-company-employees-snackbar__details-highlight").href
@@ -205,58 +185,51 @@ const getIdFromUrl = async (url, tab) => {
 }
 
 ;(async () => {
-	let db = await utils.getDb(DB_NAME)
+	let { sessionCookie, spreadsheetUrl, companies, columnName, numberOfPagePerCompany, numberOfCompaniesPerLaunch, csvName } = utils.validateArguments()
+	if (!csvName) {
+		csvName = "result"
+	}
+	let results = await utils.getDb(csvName + ".csv")
+	if (typeof spreadsheetUrl === "string") {
+		if (spreadsheetUrl.includes("linkedin.com/company")) {
+			companies = [ spreadsheetUrl ]
+		} else {
+			companies = await utils.getDataFromCsv2(spreadsheetUrl, columnName)
+			
+		}
+	}
+	if (companies.length > 1) {
+		companies = companies.filter(str => str && utils.checkDb(str, results, "query")).slice(0, numberOfCompaniesPerLaunch)
+		if (companies.length < 1) {
+			utils.log("Spreadsheet is empty or all companies from this sheet are already scraped.", "warning")
+			nick.exit()
+		}
+	}
+	if (numberOfCompaniesPerLaunch === 0) {
+		numberOfCompaniesPerLaunch = companies.length
+	}
+	console.log(`Companies to scrape: ${JSON.stringify(companies.slice(0, 500), null, 4)}`)
 	const tab = await nick.newTab()
-	let [sessionCookie, urls, numberOfPagePerCompany, waitTime, numberOfCompanyPerLaunch] = utils.checkArguments([
-		{ name: "sessionCookie", type: "string", length: 10 },
-		{ many: [
-			{ name: "companiesUrl", type: "object", length: 1 },
-			{ name: "spreadsheetUrl", type: "string", length: 10 },
-		] },
-		{ name: "numberOfPagePerCompany", type: "number", default: 10 },
-		{ name: "waitTime", type: "number", default: 2000 },
-		{ name: "numberOfCompanyPerLaunch", type: "number", default: 0 },
-	])
-
-	if (typeof urls === "string") {
-		urls = await utils.getDataFromCsv2(urls)
-	}
-
-	if (numberOfCompanyPerLaunch === 0) {
-		numberOfCompanyPerLaunch = urls.length
-	}
-
-	urls = urls.filter(el => db.findIndex(line => el === line.companyUrl) < 0).slice(0, numberOfCompanyPerLaunch)
-	if (urls.length < 1) {
-		utils.log("Spreadsheet is empty or all companies from this sheet are already scraped.", "warning")
-		nick.exit()
-	}
-	// urls = getUrlsToAdd(urls.filter(str => filterUrls(str, db)), numberOfCompanyPerLaunch)
-
 	await linkedIn.login(tab, sessionCookie)
-	let result = []
-	for (const companyUrl of urls) {
+	let currentResult = []
+	for (const query of companies) {
 		const timeLeft = await utils.checkTimeLeft()
 		if (!timeLeft.timeLeft) {
 			utils.log(`Stopped getting companies employees: ${timeLeft.message}`, "warning")
 			break
 		}
 		try {
-			const id = await getIdFromUrl(companyUrl, tab)
-			const res = await getEmployees(tab, id, numberOfPagePerCompany, waitTime)
-			res.url = companyUrl
-			result.push(res)
+			const id = await getIdFromUrl(query, tab)
+			const res = await getEmployees(tab, id, query, numberOfPagePerCompany)
+			currentResult = currentResult.concat(res)
 		} catch (error) {
-			utils.log(`Could not scrape company ${companyUrl} because ${error}`, "error")
+			utils.log(`Could not scrape company ${query} because ${error}`, "error")
 			// Saving bad entries in order to not retry on next launch
-			result.push({ url: companyUrl, employees: [{ url: "none", name: "none", job: "none", location: "none", currentJob: "none", companyUrl: "none" }] })
+			currentResult.push({ query, error, timestamp: (new Date()).toISOString() })
 		}
 	}
-	db = db.concat(jsonToCsv(result))
-	const csvResult = db
-	await linkedIn.saveCookie()
-	await utils.saveResult(csvResult)
-	// await utils.saveResults(result, csvResult, "result", ["url", "name", "job", "location", "currentJob", "companyUrl"])
+	results.push(...utils.filterRightOuter(results, currentResult))
+	await utils.saveResults(results, results, csvName)
 	nick.exit()
 })()
 	.catch(err => {
