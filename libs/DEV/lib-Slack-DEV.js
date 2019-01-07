0244647c7e84cd3ae6fab2b4159da8a59cd9c7c0
Martin Barreau <martin.barreau@gmail.com>
2019-01-07 17:08:43 +0100

    SUX API / lib-Slack: using Puppeteer instead of NickJS

--- libs/DEV/lib-Slack-DEV.js
+++ libs/DEV/lib-Slack-DEV.js
@@ -8,9 +8,9 @@ const isUrl = url => {
 	}
 }
 
+
 class Slack {
-	constructor(nick, buster, utils) {
-		this.nick = nick
+	constructor(buster, utils) {
 		this.buster = buster
 		this.utils = utils
 	}
@@ -18,48 +18,49 @@ class Slack {
 	/**
 	 * @async
 	 * @description Log into a Slack workspace with a specific session cookie
-	 * @param {Object} tab - NickJS tab instance
+	 * @param {Page} tab - Puppeteer Page instance
 	 * @param {String} url - Slack Workspace URL
 	 * @param {String} dCookie - Slack session cookie named "d"
 	 * @throws String on CSS selectors failures / BAD session cookie value
 	 */
-	async login(tab, url, dCookie) {
-		const _scrapeUsername = (arg, cb) => cb(null, document.querySelector("span#team_menu_user_name").textContent.trim())
+	async login(page, url, dCookie) {
 		const _login = async () => {
-			const [ httpCode ] = await tab.open(url)
-			if (httpCode && httpCode !== 200) {
-				return `Slack responded with ${httpCode}`
+			const response = await page.goto(url, { timeout: 30000, waitUntil: "load" })
+			if (response.status() !== 200)
+				return `Slack responsed with ${response.status()}`
+			try {
+				await Promise.all([ page.waitForSelector("div#team_menu", { timeout: 30000 }), page.waitFor(() => !document.querySelector("body").classList.contains("loading")) ])
+				const name = await page.evaluate(() => document.querySelector("span#team_menu_user_name").textContent.trim())
+				this.utils.log(`Connected as ${name}`, "done")
+			} catch (err) {
+				await page.screenshot({ path: `err-login-${Date.now()}.jpg`, type: "jpeg", quality: 50 })
+				this.utils.log(`Error: ${err.message || err}`, "warning")
 			}
-			// Need to wait until both the sidebar AND the username are loaded by Slack
-			await tab.waitUntilVisible([ "div#team_menu", "span#team_menu_user_name" ], 30000, "and")
-			const name = await tab.evaluate(_scrapeUsername)
-			this.utils.log(`Connected as ${name}`, "done")
-		}
 
+		}
 		if (typeof dCookie !== "string" || dCookie.trim().length < 1) {
 			this.utils.log("Invalid Slack session cookie. Did you specify the \"d\" cookie?", "warning")
-			this.nick.exit(this.utils.ERROR_CODES.SLACK_BAD_COOKIE)
+			process.exit(this.utils.ERROR_CODES.SLACK_BAD_COOKIE)
 		}
 
 		if (typeof url !== "string" || url.trim().length < 1 || !isUrl(url)) {
 			this.utils.log("Invalid Slack Workspace URL. Did you specify one?", "warning")
-			this.nick.exit(this.utils.ERROR_CODES.SLACK_BAD_WORKSPACE)
+			process.exit(this.utils.ERROR_CODES.SLACK_BAD_WORKSPACE)
 		}
 
 		if (url === "slack_workspace_url") {
 			this.utils.log("", "warning")
-			this.nick.exit(this.utils.ERROR_CODES.SLACK_DEFAULT_WORKSPACE)
+			process.exit(this.utils.ERROR_CODES.SLACK_DEFAULT_WORKSPACE)
 		}
-
 
 		if (dCookie === "d_cookie") {
 			this.utils.log("You didn't set the Slack \"d\" cookie in your API configuration", "warning")
-			this.nick.exit(this.utils.ERROR_CODES.SLACK_DEFAULT_COOKIE)
+			process.exit(this.utils.ERROR_CODES.SLACK_DEFAULT_COOKIE)
 		}
 
 		this.utils.log("Connecting to Slack...", "loading")
 		try {
-			await this.nick.setCookie({
+			await page.setCookie({
 				name: "d",
 				value: dCookie,
 				domain: ".slack.com",
@@ -69,27 +70,20 @@ class Slack {
 			await _login()
 		} catch (err) {
 			this.utils.log("Could not connect to Slack with this session cookie", "error")
-			this.nick.exit(this.utils.ERROR_CODES.SLACK_BAD_COOKIE)
+			process.exit(this.utils.ERROR_CODES.SLACK_BAD_COOKIE)
 		}
 	}
 
-	/**
-	 * @async
-	 * @description Get the channels list from a specific workspace
-	 * @param {Object} tab - NickJS tab instance with a slack session
-	 * @return {Promise<Array<Object>>} Channels found
-	 */
-	async getChannelsMeta(tab) {
+	async getChannelsMeta(page) {
 		/* global slackDebug */
-		const getSlackObject = (arg, cb) => {
-			if (!slackDebug) {
-				return cb(null)
-			}
+		const getSlackObject = field => {
+			if (!slackDebug)
+				return Promise.resolve(null)
 			const store = slackDebug.storeInstance.getStateByTeamId(slackDebug.activeTeamId)
-			cb(null, store[arg.field])
+			return Promise.resolve(store[field])
 		}
-		const channelsObject = await tab.evaluate(getSlackObject, { field: "channels" })
-		const membersObject = await tab.evaluate(getSlackObject, { field: "members" })
+		const channelsObject = await page.evaluate(getSlackObject, "channels")
+		const membersObject = await page.evaluate(getSlackObject, "members")
 
 		const channels = []
 		for (const one of Object.keys(channelsObject)) {
--- store/DEV/Slack User Extractor DEV/Slack User Extractor DEV.js
+++ store/DEV/Slack User Extractor DEV/Slack User Extractor DEV.js
@@ -2,29 +2,21 @@
 "phantombuster command: nodejs"
 "phantombuster package: 5"
 "phantombuster dependencies: lib-StoreUtilities-DEV.js, lib-Slack-DEV.js"
-"phantombuster flags: save-folder"
 
 const { URL } = require("url")
+
+const Puppeteer = require("puppeteer")
 
 const Buster = require("phantombuster")
 const buster = new Buster()
 
-const Nick = require("nickjs")
-const nick = new Nick({
-	loadImages: true,
-	printPageErrors: false,
-	printResourceErrors: false,
-	printNavigation: false,
-	printAborts: false,
-	debug: false,
-	timeout: 30000
-})
+const nick = null // TODO: remove this hack to not make utils throw errors
 
 const StoreUtilities = require("./lib-StoreUtilities-DEV")
 const utils = new StoreUtilities(nick, buster)
 
 const Slack = require("./lib-Slack-DEV")
-const slack = new Slack(nick, buster, utils)
+const slack = new Slack(buster, utils)
 
 const DEFAULT_DB = "result"
 const DEFAULT_LAUNCH = 1
@@ -33,28 +25,8 @@ const DEFAULT_LAUNCH = 1
 /**
  * @return {Promise<String|null>}
  */
-const getChannelName = (arg, cb) => cb(null, document.querySelector("button#channel_title") ? document.querySelector("button#channel_title").textContent.trim() : null)
+const getChannelName = () => Promise.resolve(document.querySelector("button#channel_title") ? document.querySelector("button#channel_title").textContent.trim() : null)
 
-/**
- * @param { { chanName: String } } arg
- * @return {Promise<Boolean>}
- * @throws String when the switch is finished after 30s
- */
-const waitWhileChanSwitch = (arg, cb) => {
-	const idleStarted = Date.now()
-	const idle = () => {
-		const chanName = document.querySelector("button#channel_title")
-
-		if (!chanName || (chanName.textContent.trim() === arg.chanName)) {
-			if (Date.now() - idleStarted >= 30000) {
-				return cb(`Still in channel ${arg.chanName} after 30s`)
-			}
-			setTimeout(idle, 100)
-		}
-		cb(null, true)
-	}
-	idle()
-}
 
 /**
  * @param {String} workspace - Slack Workspace URL
@@ -71,21 +43,33 @@ const forgeChannelUrl = (workspace, chanId) => {
 	}
 }
 
+/**
+ * @param {String} oldChanName
+ * @return {Boolean} true if a new channel was loaded othrewise false
+ */
+const waitWhileSwitching = oldChanName => {
+	const loadedName = document.querySelector("button#channel_title")
+	if (!loadedName || (loadedName.textContent.trim() === oldChanName))
+		return false
+	return true
+}
+
 const switchChannel = async (tab, workspaceUrl, channelId) => {
 	let channelUrl = forgeChannelUrl(workspaceUrl, channelId)
 	const chanName = await tab.evaluate(getChannelName)
 
-	await tab.open(channelUrl)
-	await tab.evaluate(waitWhileChanSwitch, { chanName })
-	await tab.waitUntilVisible([ "div#team_menu", "span#team_menu_user_name" ], 30000, "and")
-	await tab.screenshot(`switch-${Date.now()}.jpg`)
+	await tab.goto(channelUrl)
+	await tab.waitFor(waitWhileSwitching, { timeout: 30000 } , chanName)
+	Promise.all([ tab.waitForSelector("div#team_menu", { timeout: 30000 }), tab.waitFor(() => !document.querySelector("body").classList.contains("loading")) ])
+	await tab.screenshot({ path: `switch-${Date.now()}.jpg`, type: "jpeg", quality: 50 })
 }
 
 ;(async () => {
 	let db = null
 	const res = []
 	let { sessionCookie, slackWorkspaceUrl, spreadsheetUrl, columnName, numberOfLinesPerLaunch, csvName, queries } = utils.validateArguments()
-	const tab = await nick.newTab()
+	const Browser = await Puppeteer.launch({ args: [ "--no-sandbox" ] })
+	const tab = await Browser.newPage()
 
 	if (!csvName) {
 		csvName = DEFAULT_DB
@@ -111,10 +95,10 @@ const switchChannel = async (tab, workspaceUrl, channelId) => {
 	}
 
 	await utils.saveResults(res, res, csvName, null)
-	nick.exit()
+	process.exit()
 })()
 .catch(err => {
 	utils.log(`API execution error: ${err.message || err}`, "error")
 	console.log(err.stack || "no stack")
-	nick.exit(1)
+	process.exit(1)
 })
