9db43f3656099b50cbf27bdb82fd2d9f02519ff7
Nathan Gerszanowilsz <fffan@free.fr>
2019-01-18 17:13:05 +0100

    PHUE: new dev version

--- store/DEV/Product Hunt Upvoter Extractor DEV/Product Hunt Upvoter Extractor DEV.ts
+++ store/DEV/Product Hunt Upvoter Extractor DEV/Product Hunt Upvoter Extractor DEV.ts
@@ -32,27 +32,52 @@ const getProfilesCount = (): number => {
 	return document.querySelectorAll("div[class*=popoverContent] > div").length
 }
 
-const scrapeProfiles = (query: string, currentUrl: string): IEvalAny => {
-	const results = Array.from(document.querySelectorAll("div[class*=popoverContent] > div"))
-	const scrapedData = []
-	for (const result of results) {
-		const profile = { query, timestamp: (new Date()).toISOString() } as IUnknownObject
-		const profileUrl = result.querySelector("a")
-		if (profileUrl) {
-			profile.profileUrl = profileUrl.href
+const scrapeProfile = (query: string, currentUrl: string, position: number): IEvalAny => {
+	const result = document.querySelector(`div[class*="popoverContent"] > div:nth-child(${position + 1})`)
+	const profile = { query, productUrl: currentUrl, timestamp: (new Date()).toISOString(), position: position + 1 } as IUnknownObject
+	if (result) {
+		const profileSelector = result.querySelector("a")
+		if (profileSelector) {
+			const profileUrl = profileSelector.href
+			profile.profileUrl = profileUrl
+			if (profileUrl.indexOf("@") > -1) {
+				profile.profileHandle = profileUrl.slice(profileUrl.indexOf("@"))
+			}
 		}
 		const imgUrl = result.querySelector("img")
 		if (imgUrl) {
-			profile.name = imgUrl.alt
+			const fullName = imgUrl.alt
+			profile.fullName = fullName
+			const nameArray = fullName.split(" ")
+			const firstName = nameArray.shift()
+			const lastName = nameArray.join(" ")
+			profile.firstName = firstName
+			if (lastName) {
+				profile.lastName = lastName
+			}
 			const imgObject = new URL(imgUrl.src)
 			profile.imgUrl = imgObject.hostname + imgObject.pathname
 		}
-		scrapedData.push(profile)
 	}
-	return scrapedData
+	return profile
 }
 
-const openProfile = async (page: puppeteer.Page, url: string, query: string) => {
+const scrollProfiles = (position: number) => {
+	const lastProfile = document.querySelector(`[class*="popoverContent"] > div:nth-child(${position})`)
+	if (lastProfile) {
+		lastProfile.scrollIntoView()
+	}
+}
+
+const getUpvoteCount = () => {
+	const upvoteButton = document.querySelector("[class*=bigButtonCount]")
+	if (upvoteButton && upvoteButton.textContent) {
+		return parseInt(upvoteButton.textContent.replace(/\D+/g, ""), 10)
+	}
+	return 0
+}
+
+const openProfile = async (page: puppeteer.Page, maxProfiles: number, url: string, query: string) => {
 	const response = await page.goto(url)
 	if (response && response.status() !== 200) {
 		throw new Error(`${url} responded with HTTP code ${response.status()}`)
@@ -60,33 +85,60 @@ const openProfile = async (page: puppeteer.Page, url: string, query: string) => 
 	await page.waitForSelector("div[class*=voters]")
 	await page.waitFor(2000)
 	await page.click("div[class*=voters]")
-	// await page.evaluate(() => {
-	// 	const button = document.querySelector("div[class*=voters]") as HTMLElement
-	// 	if (button) {
-	// 		button.click()
-	// 	}
-	// })
+
 	await page.waitForSelector("div[data-test=\"popover\"]")
 	await page.waitFor(1000)
-	const profileCount = await page.evaluate(getProfilesCount)
-	console.log("count", profileCount)
+	let profileCount: number = 0
+	const totalUpvoteCount = await page.evaluate(getUpvoteCount) as number
+	if (totalUpvoteCount && totalUpvoteCount < maxProfiles) {
+		maxProfiles = totalUpvoteCount
+	}
+	let lastDate = new Date().getTime()
+	let profilesArray = []
+	do {
+		const timeLeft = await utils.checkTimeLeft()
+		if (!timeLeft.timeLeft) {
+			utils.log(timeLeft.message, "warning")
+			break
+		}
+		try {
+			const newProfileCount = await page.evaluate(getProfilesCount) as number
+			if (newProfileCount > profileCount) {
+			for (let i = profileCount; i < newProfileCount; i++) {
+				await page.evaluate(scrollProfiles, i)
+				const profileData = await page.evaluate(scrapeProfile, query, url, i)
+				profilesArray.push(profileData)
+				// await page.waitFor(100)
+			}
+			lastDate = new Date().getTime()
+			profileCount = newProfileCount
+			utils.log(`Loaded ${Math.min(profileCount, maxProfiles)} profiles.`, "done")
+		}
+		} catch (err) {
+			utils.log(`Error during scraping: ${err}`, "error")
+		}
+		if (new Date().getTime() - lastDate > 30000) {
+			utils.log("Took too long to load the rest of them!", "warning")
+			break
+		}
+	} while (profileCount < maxProfiles)
 
-	const profileData = await page.evaluate(scrapeProfiles, query)
-
-	await page.screenshot({ path: `${Date.now()}profile.jpg`, type: "jpeg", quality: 50 })
-
-	return profileData
+	// await page.screenshot({ path: `${Date.now()}profile.jpg`, type: "jpeg", quality: 50 })
+	// await buster.saveText(await page.evaluate(() => document.body.innerHTML) as string, `${Date.now()}profile.html`)
+	profilesArray = profilesArray.slice(0, maxProfiles)
+	utils.log(`Got ${profilesArray.length} profiles.`, "done")
+	return profilesArray
 }
 
 (async () => {
 	const browser = await puppeteer.launch({ args: [ "--no-sandbox" ] })
 	const page = await browser.newPage()
-	const { spreadsheetUrl, columnName, numberOfLinesPerLaunch, csvName, profileUrls } = utils.validateArguments()
+	const { spreadsheetUrl, numberOfProfilesPerProduct, columnName, numberOfLinesPerLaunch, csvName, profileUrls } = utils.validateArguments()
 	let profileArray = []
 	const inputUrl = spreadsheetUrl as string
 	let _csvName = csvName as string
 	const _columnName = columnName as string
-
+	const maxProfiles = numberOfProfilesPerProduct as number
 	let numberOfLines = numberOfLinesPerLaunch as number
 	if (_csvName) {
 		_csvName = DB_NAME
@@ -105,7 +157,7 @@ const openProfile = async (page: puppeteer.Page, url: string, query: string) => 
 	if (typeof profileUrls === "string") {
 		profileArray = [ profileUrls ]
 	}
-	const result = await utils.getDb(csvName + ".csv")
+	let result = await utils.getDb(csvName + ".csv")
 
 	if (Array.isArray(profileUrls)) {
 		profileArray = profileUrls.filter((el) => result.findIndex((line: IUnknownObject) => line.query === el) < 0)
@@ -113,7 +165,6 @@ const openProfile = async (page: puppeteer.Page, url: string, query: string) => 
 			profileArray = profileArray.slice(0, numberOfLines)
 		}
 	}
-	console.log("profileU", profileArray)
 	if (Array.isArray(profileArray)) {
 		if (profileArray.length < 1) {
 			utils.log("Input is empty OR every profiles are already scraped", "warning")
@@ -125,10 +176,10 @@ const openProfile = async (page: puppeteer.Page, url: string, query: string) => 
 			const url = utils.isUrl(query) ? query : `https://www.producthunt.com/${query}`
 			let res = null
 			try {
-				res = await openProfile(page, url, query) as ReturnType <typeof scrapeProfiles>
+				res = await openProfile(page, maxProfiles, url, query)
 				if (res) {
 					utils.log(`${query} scraped`, "done")
-					result.push(res)
+					result = result.concat(res)
 				}
 			} catch (err) {
 				const error = `Error while scraping ${url}: ${err.message || err}`
