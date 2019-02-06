a383d0028879b2022037fe8dd1affc5399240483
Nathan Gerszanowilsz <fffan@free.fr>
2019-02-06 19:28:17 +0100

    IPE: prod version

--- store/DEV/Instagram Posts Extractor DEV/Instagram Posts Extractor DEV.js
+++ store/DEV/Instagram Posts Extractor DEV/Instagram Posts Extractor DEV.js
@@ -24,7 +24,7 @@ const utils = new StoreUtilities(nick, buster)
 const Instagram = require("./lib-Instagram-DEV")
 const instagram = new Instagram(nick, buster, utils)
 let graphqlUrl
-let headers
+let gotAllPosts
 // }
 
 const getUrlsToScrape = (data, numberOfProfilesPerLaunch) => {
@@ -47,16 +47,15 @@ const checkRateLimit = (arg, cb) => {
 }
 
 const interceptInstagramApiCalls = e => {
-	if (e.response.url.includes("graphql/query/?query_hash") && e.response.status === 200) {
+	if (!graphqlUrl && e.response.url.includes("graphql/query/?query_hash") && e.response.url.includes("first") && !e.response.url.includes("include_suggested_users") && e.response.status === 200) {
 		graphqlUrl = e.response.url
 		console.log("graphqlUrl", graphqlUrl)
 	}
 }
 
-const onHttpRequest = (e) => {
-	if (e.request.url.indexOf("graphql/query/?query_hash")) {
-		headers = e.request.headers
-	}
+const forgeNewUrl = (endCursor) => {
+	const newUrl = graphqlUrl.slice(0, graphqlUrl.indexOf("first")) + encodeURIComponent("first\":50,\"after\":\"") + endCursor + encodeURIComponent("\"}")
+	return newUrl
 }
 
 const getPosts = async (tab, profileUrl, query) => {
@@ -73,13 +72,46 @@ const getPosts = async (tab, profileUrl, query) => {
 		await tab.wait(1000)
 		await tab.scrollToBottom()
 	} while (!graphqlUrl)
+	if (!graphqlUrl) {
+		utils.log("Got all posts or Instagram took too long to load them.", "done")
+		return []
+	}
+	let newUrl = graphqlUrl
+	const results = []
+	do {
+		await tab.open(newUrl)
+		let instagramJsonCode = await tab.getContent()
+		const partCode = instagramJsonCode.slice(instagramJsonCode.indexOf("{"))
+		instagramJsonCode = JSON.parse(partCode.slice(0, partCode.indexOf("<")))
+		// console.log("instagramJsonCode", instagramJsonCode.data.user.edge_owner_to_timeline_media.edges)
+		console.log("iJ:", instagramJsonCode.data.user)
+		const endCursor = instagramJsonCode.data.user.edge_owner_to_timeline_media.page_info.end_cursor
+
+		const posts = instagramJsonCode.data.user.edge_owner_to_timeline_media.edges
+		for (const post of posts) {
+			const scrapedData = extractPostData(post.node, profileUrl, query)
+			results.push(scrapedData)
+			console.log("rLength:", results.length)
+		}
+		if (!endCursor) {
+			console.log("no Endcursor")
+			break
+		}
+		newUrl = forgeNewUrl(endCursor)
+	} while (true)
+	return results
 }
 
 const extractPostData = (post, profileUrl, query) => {
 	const postData = { timestamp: (new Date()).toISOString(), query, profileUrl }
 	postData.postUrl = `https://www.instagram.com/p/${post.shortcode}/`
 	postData.commentCount = post.edge_media_to_comment.count
-	postData.likeCount = post.edge_liked_by.count
+	if (post.edge_liked_by) {
+		postData.likeCount = post.edge_liked_by.count
+	} else if (post.edge_media_preview_like) {
+		postData.likeCount = post.edge_media_preview_like
+	}
+
 	postData.id = post.id
 	if (post.location) {
 		postData.locationId = post.location.id
@@ -91,26 +123,27 @@ const extractPostData = (post, profileUrl, query) => {
 	return postData
 }
 
-const getFirstPosts = async (tab, profileUrl, query) => {
+const getFirstPosts = async (profileUrl, query) => {
+	const jsonTab = await nick.newTab()
 	const jsonUrl = `${profileUrl}?__a=1`
-	await tab.open(jsonUrl)
-	let instagramJsonCode = await tab.getContent()
+	await jsonTab.open(jsonUrl)
+	let instagramJsonCode = await jsonTab.getContent()
 	const partCode = instagramJsonCode.slice(instagramJsonCode.indexOf("{"))
 	instagramJsonCode = JSON.parse(partCode.slice(0, partCode.indexOf("<")))
 	const data = instagramJsonCode.graphql.user
 	const fullName = data.full_name
 	const postCount = data.edge_owner_to_timeline_media.count
 	utils.log(`${fullName} has ${postCount} posts.`, "info")
-	console.log("data:", data)
+	// console.log("data:", data)
 
 	const firstPosts = data.edge_owner_to_timeline_media.edges
 	const firstResults = []
 	for (const post of firstPosts) {
 		const scrapedData = extractPostData(post.node, profileUrl, query)
-		console.log("p:", post)
+		// console.log("p:", post)
 		firstResults.push(scrapedData)
 	}
-	return firstResults
+	return { firstResults, fullName, postCount }
 }
 
 // Main function that execute all the steps to launch the scrape and handle errors
@@ -155,10 +188,10 @@ const getFirstPosts = async (tab, profileUrl, query) => {
 
 	console.log(`URLs to scrape: ${JSON.stringify(profileUrls, null, 4)}`)
 	tab.driver.client.on("Network.responseReceived", interceptInstagramApiCalls)
-	tab.driver.client.on("Network.requestWillBeSent", onHttpRequest)
 	let pageCount = 0
 	let tempResult = []
 	for (const query of profileUrls) {
+		gotAllPosts = false
 		const timeLeft = await utils.checkTimeLeft()
 		if (!timeLeft.timeLeft) {
 			utils.log(`Scraping stopped: ${timeLeft.message}`, "warning")
@@ -176,8 +209,12 @@ const getFirstPosts = async (tab, profileUrl, query) => {
 				continue
 			}
 			const profileUrl = await tab.getUrl()
-			tempResult = await getFirstPosts(tab, profileUrl, query)
-			// const tempResult = await getPosts(tab, profileUrl, query)
+			const firstPosts = await getFirstPosts(profileUrl, query)
+			tempResult = firstPosts.firstResults
+			if (tempResult.length !== firstPosts.postCount) {
+				tempResult = tempResult.concat(await getPosts(tab, profileUrl, query))
+			}
+			utils.log(`Got ${tempResult.length} posts from ${firstPosts.fullName}.`, "done")
 		} catch (err) {
 			try {
 				await tab.waitUntilVisible("body")
@@ -194,13 +231,12 @@ const getFirstPosts = async (tab, profileUrl, query) => {
 		await tab.wait(2500 + Math.random() * 2000)
 	}
 	for (let i = 0; i < tempResult.length; i++) {
-		if (!result.find(el => el.instagramID === tempResult[i].instagramID && el.query === tempResult[i].query)) {
+		if (!result.find(el => el.postUrl === tempResult[i].postUrl)) {
 			result.push(tempResult[i])
 		}
 	}
 	await utils.saveResults(result, result, csvName)
 	tab.driver.client.removeListener("Network.responseReceived", interceptInstagramApiCalls)
-	tab.driver.client.removeListener("Network.requestWillBeSent", onHttpRequest)
 	nick.exit(0)
 })()
 .catch(err => {
