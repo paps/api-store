a60af58b3c59a2b9cdaff46b40d0d55c72edbebb
Mehdi Ait-Bachir <mehdi.aitbachir@phantombuster.com>
2019-03-28 17:12:14 +0100

    Added v2 test cson and a shell script to upload all APIs

---
+++ upload-all.sh
@@ -0,0 +1,9 @@
+csonFile=${1:-"v2-test.cson"}
+jsFileList=$( cat "$csonFile" | sed -e '1,/scripts:/d' | grep -v \# | grep -v \] | sed '/^[[:space:]]*$/d' | cut -d: -f2 | sed "s/'//g" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' )
+mdFileList=$( echo "$jsFileList" | sed 's/\(.*\.\)js/\1md/' )
+jsonFileList=$( echo "$jsFileList" | sed 's/\(.*\.\)js/\1json/' )
+allFilesList=$( echo "$jsFileList\n$mdFileList\n$jsonFileList" )
+IFS=$'\n'
+for file in $allFilesList; do
+    phantombuster -c $csonFile "$file"
+done
