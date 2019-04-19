474b75dbdddb8c9b3343d5d96b32ee5e059d75bf
Nath' Nathan <nathan.gerszanowilsz@gmail.com>
2019-04-19 12:11:15 +0200

    LSNEF: prod version

---
+++ store/LinkedIn Sales Navigator Email Finder/LinkedIn Sales Navigator Email Finder.js
@@ -0,0 +1,275 @@
+// Phantombuster configuration {
+"phantombuster command: nodejs"
+"phantombuster package: 5"
+"phantombuster dependencies: lib-StoreUtilities.js, lib-Hunter.js, lib-Dropcontact.js, lib-DiscoverMail.js"
+"phantombuster flags: save-folder"
+
+const Buster = require("phantombuster")
+const buster = new Buster()
+
+const StoreUtilities = require("./lib-StoreUtilities")
+const utils = new StoreUtilities(buster)
+// }
+
+const cleanObject = (obj) => {
+	for (const key of Object.keys(obj)) {
+		if (obj[key] === null) {
+			delete obj[key]
+		} else if (obj[key].length === 0) {
+			delete obj[key]
+		}
+	}
+}
+
+
+;(async () => {
+	let { spreadsheetUrl, emailChooser, hunterApiKey, dropcontactApiKey, columnName, csvName, numberOfLinesPerLaunch } = utils.validateArguments()
+	if (spreadsheetUrl && spreadsheetUrl.includes("linkedin.com/sales/search/")) {
+		throw "Input URL is a Search URL and not a Spreadsheet URL. Please use LinkedIn Sales Navigator Search Export first to extract profiles from a search URL."
+	}
+	if (!csvName) { csvName = "result" }
+	let result = await utils.getDb(csvName + ".csv")
+	let dropcontact
+	let hunter
+	let phantombusterMail
+	if (emailChooser === "phantombuster") {
+		require("coffee-script/register")
+		phantombusterMail = new (require("./lib-DiscoverMail"))(buster.apiKey)
+	}
+	if ((typeof(hunterApiKey) === "string") && (hunterApiKey.trim().length > 0)) {
+		require("coffee-script/register")
+		hunter = new (require("./lib-Hunter"))(hunterApiKey.trim())
+	}
+	if ((typeof(dropcontactApiKey) === "string") && (dropcontactApiKey.trim().length > 0)) {
+		require("coffee-script/register")
+		dropcontact = new (require("./lib-Dropcontact"))(dropcontactApiKey.trim())
+	}
+	let csv, header
+	let queries = []
+	let firstNameIndex = -1
+	let lastNameIndex = -1
+	let fullNameIndex = -1
+	let companyNameIndex = -1
+	let domainIndex = -1
+	try {
+		csv = await utils.getRawCsv(spreadsheetUrl)
+		header = csv[0]
+		if (((header.includes("firstName") && header.includes("lastName")) || header.includes("name") || header.includes("fullName")) && (header.includes("companyName") || header.includes("company") || header.includes("website"))) {
+			if (header.includes("firstName")) {
+				firstNameHeader = "firstName"
+				firstNameIndex = header.indexOf("firstName")
+			}
+			if (header.includes("lastName")) {
+				lastNameHeader = "lastName"
+				lastNameIndex = header.indexOf("lastName")
+			}
+			if (header.includes("name")) {
+				fullNameHeader = "name"
+				fullNameIndex = header.indexOf("name")
+			}
+			if (header.includes("fullName")) {
+				fullNameHeader = "fullName"
+				fullNameIndex = header.indexOf("fullName")
+			}
+			if (header.includes("companyName")) {
+				companyNameHeader = "companyName"
+				companyNameIndex = header.indexOf("companyName")
+			}
+			if (header.includes("company")) {
+				companyNameHeader = "company"
+				companyNameIndex = header.indexOf("company")
+			}
+			if (header.includes("website")) {
+				domainHeader = "website"
+				domainIndex = header.indexOf("website")
+			}
+			console.log("csv:", csv)
+			console.log("fullNameIndex", fullNameIndex)
+			for (let i = 1; i < csv.length; i++) {
+				const csvObject = csv[i]
+				console.log("csvObject:", csvObject[0])
+				let fullName = fullNameIndex > -1 ? csvObject[fullNameIndex] : null
+				const firstName = firstNameIndex > -1 ? csvObject[firstNameIndex] : null
+				const lastName = lastNameIndex > -1 ? csvObject[lastNameIndex] : null
+				const companyName = companyNameIndex > -1 ? csvObject[companyNameIndex] : null
+				const domain = domainIndex > -1 ? csvObject[domainIndex] : null
+				console.log("fullName:", fullName)
+				if (!fullName) {
+					fullName = firstName + lastName
+				}
+				console.log("fn:", fullName)
+				const queryCheck = `${fullName} | ${companyName ? companyName : ""} | ${domain ? domain : ""}`
+				console.log("queryCheck:", queryCheck)
+				let found = false
+				for (const line of result) {
+					if (line.query === queryCheck) {
+						found = true
+						break
+					}
+				}
+				if (!found) {
+					queries.push(csvObject)
+					console.log("on push")
+				}
+				if (numberOfLinesPerLaunch && queries.length === numberOfLinesPerLaunch) {
+					console.log("on break")
+					break
+				}
+			}
+			if (queries.length === 0) {
+				utils.log("Input spreadsheet is empty OR we already processed all the profiles from this spreadsheet.", "warning")
+				process.exit(1)
+			}
+			console.log("on a break")
+		} else {
+			utils.log("Spreadsheet non valid, please manually specify which columns to get the data from.", "error")
+			process.exit(1)
+		}
+	} catch (err) {
+		utils.log(`Couldn't access the spreadsheet: ${err}`, "error")
+		process.exit(1)
+	}
+	const currentResult = []
+	for (const csvObject of queries) {
+		let resultObject = {}
+		const finalObject = {}
+		for (let i = 0; i < header.length; i++) {
+			finalObject[header[i]] = csvObject[i]
+		}
+		// console.log("csvObject", csvObject)
+		let fullName = fullNameIndex > -1 ? csvObject[fullNameIndex] : null
+		const firstName = firstNameIndex > -1 ? csvObject[firstNameIndex] : null
+		const lastName = lastNameIndex > -1 ? csvObject[lastNameIndex] : null
+		const companyName = companyNameIndex > -1 ? csvObject[companyNameIndex] : null
+		const domain = domainIndex > -1 ? csvObject[domainIndex] : null
+		const mailPayload = {}
+		if (firstName && lastName) {
+			mailPayload.first_name = firstName
+			mailPayload.last_name = lastName
+		} else {
+			full_name = fullName
+		}
+		if (!fullName) {
+			fullName = firstName + lastName
+		}
+		const query = `${fullName} | ${companyName ? companyName : ""} | ${domain ? domain : ""}`
+		if (domain) {
+			mailPayload.domain = domain
+		}
+		if (companyName) {
+			mailPayload.company = companyName
+		}
+		console.log("mailPayload:", mailPayload)
+		if (mailPayload.domain || mailPayload.company) {
+			if (hunter) {
+				try {
+					if (!mailPayload.first_name && !mailPayload.last_name) {
+						console.log("fj", fullName)
+						const nameArray = fullName.split(" ")
+						mailPayload.first_name = nameArray.shift()
+						const last_name = nameArray.join(" ")
+						if (last_name) {
+							mailPayload.last_name = last_name
+						}
+					}
+					resultObject = await hunter.find(mailPayload)
+					utils.log(`Hunter found ${resultObject.email || "nothing"} for ${fullName} working at ${companyName || domain}`, "info")
+					if (!resultObject.email) {
+						resultObject.error = "No mail found"
+					} else {
+						resultObject.emailFromHunter = resultObject.email
+						delete resultObject.email
+					}
+					if (dropcontact) {
+						cleanObject(resultObject)
+						Object.assign(finalObject, resultObject)
+					}
+				} catch (err) {
+					utils.log(err, "error")
+					if (err.message === "Hunter.io: got HTTP 401 - No user found for the API key supplied") {
+						break
+					}
+				}		
+			}
+			if (dropcontact) {
+				try {
+					resultObject = await dropcontact.clean(mailPayload)
+					console.log("drp", resultObject)
+					utils.log(`Dropcontact found ${resultObject.email || "nothing"} for ${fullName} working at ${companyName || domain}`, "info")
+					if (!resultObject.email) {
+						resultObject.error = "No mail found"
+					} else {
+						resultObject.emailFromDropcontact = resultObject.email
+						delete resultObject.email
+					}
+				} catch (err) {
+					utils.log(err, "error")
+					if (err.message = "Dropcontact returned HTTP 401") {
+						break
+					}
+				}				
+			}
+			if (phantombusterMail) {
+				mailPayload.siren = true
+				let init = new Date()
+				let status = ""
+				try {
+					const dropcontactSearch = await phantombusterMail.find(mailPayload)
+					resultObject = dropcontactSearch.data
+					utils.log(`Phantombuster via Dropcontact found ${resultObject.email || "nothing"} for ${fullName} working at ${companyName || domain }`, "info")
+					if (resultObject.email) {
+						const qualification = resultObject["email qualification"]
+						status = `Found ${qualification}`
+					} else {
+						resultObject.error = "No mail found"
+						// currentResult.push({ query, error: "No mail found", timestamp: (new Date().toISOString()) })
+						status = "Not found"
+					}
+				} catch (err) {
+					utils.log(`Phantombuster via Dropcontact didn't find anything for ${fullName} working at ${companyName || domain }`, "info")
+					console.log("err:", err)
+					resultObject.error = "No mail found"
+					status = err.message
+				}
+				try {
+					const needle = require("needle")
+					const options = {
+						headers:  {
+							"Content-Type": "application/x-www-form-urlencoded",
+						}
+					}
+					const os = require("os")
+					const hostname = os.hostname()
+					const user_id = `dropcontact_${hostname}`
+					const event_type = "email_request"
+					const apiKey = "5f442f063c9d596a7157f248f1010e1a"
+					await needle("post", "https://api.amplitude.com/httpapi",`api_key=${apiKey}&event=[{"user_id":"${user_id}", "event_type":"${event_type}", "event_properties":{"status": "${status}"}}]`, JSON.stringify(options))
+				} catch (err) {
+					console.log("err:", err)
+				}
+			}
+		} else {
+			utils.log("Can't search for emails as no current company's been found!", "warning")
+			resultObject.error = "No company found"
+		}
+		cleanObject(resultObject)
+		console.log("resultO", resultObject)
+		Object.assign(finalObject, resultObject)
+		finalObject.query = query
+		finalObject.timestamp = (new Date().toISOString())
+		currentResult.push(finalObject)
+		const timeLeft = await utils.checkTimeLeft()
+		if (!timeLeft.timeLeft) {
+			utils.log(timeLeft.message, "warning")
+			break
+		}
+		buster.progressHint(i / queries.length, `${i} mail requests done`)
+	}
+	result = result.concat(currentResult)
+	await utils.saveResults(currentResult, result, csvName)
+	process.exit(0)
+})()
+	.catch(err => {
+		utils.log(err, "error")
+		process.exit(1)
+	})
---
+++ store/LinkedIn Sales Navigator Email Finder/LinkedIn Sales Navigator Email Finder.md
@@ -0,0 +1,84 @@
+# Convert your Sales Navigator URLs to default LinkedIn profile URLs
+
+Got a list of Sales Navigator profile URLs and want to their regular profile URLs? 
+Well that's all that API does :) And you don't even need a Sales Navigator account for it to work!
+Note that for converting the Sales Navigator URL, the API needs to visit the profile for that URL.
+
+# What will you need? ⚙️ 
+
+- **Session cookie**: Your session Cookie from LinkedIn
+- **Spreadsheet URL**: Your spreadsheet URL as input
+- **Number of lines to process per launch**: How many URLs to convert from your spreadsheet (all of them if empty)
+
+
+_(**You already have all that?**  Click straight away on **"Use this API"**)_
+
+
+# Which steps to follow?
+## 1. Create an account on Phantombuster.com 💻
+If you haven't already, create a **FREE** account on [Phantombuster](https://phantombuster.com/register). Our service will browse the web for you. It’s a website automator which runs in the cloud. Once done we'll follow up.
+
+
+## 2. Use this API on your account.👌
+Now that you're connected to Phantombuster, Click on the following button (it will open a new tab).
+
+<center><button type="button" class="btn btn-warning callToAction" onclick="useThisApi()">USE THIS API!</button></center>
+
+## 3. Click on Configure me!
+You'll now see the 3 configuration dots blinking. Click on them.
+
+<center>![](https://phantombuster.imgix.net/api-store/configure_me.JPG)</center>
+
+
+## 4. Easy & safe authentication { argument }
+
+This automation will connect to LinkedIn on your behalf. The **safest and most efficient** way for Phantombuster to authenticate as yourself is by using your session cookies.
+
+To make that process as easy as possible you can use **Phantombuster's browser extension**. It's a 2-click installation.
+
+<div class="row" style="margin: 10px 0px;">
+	<div class="col-xs-5 col-xs-offset-1">
+		<a href="https://chrome.google.com/webstore/detail/phantombuster/mdlnjfcpdiaclglfbdkbleiamdafilil" 
+		target="_blank">
+			<div class="btn btn-default text-center" style="display: inline-block; align-items: center;">
+				<p style="margin-top: 0px;">
+				<img src="https://s3-eu-west-1.amazonaws.com/phantombuster-static/api-store/Browser+Extension/chrome.svg" style="height: 35px; box-shadow: 0px 0px 0px white">
+				Get it for Chrome</p>
+			</div>
+		</a>
+	</div>
+	<div class="col-xs-5 col-xs-offset-1">
+		<a href="https://addons.mozilla.org/fr/firefox/addon/phantombuster/" 
+		target="_blank">
+			<div class="btn btn-default text-center" style="display: inline-block; align-items: center;">
+				<p style="margin-top: 0px;">
+				<img src="https://s3-eu-west-1.amazonaws.com/phantombuster-static/api-store/Browser+Extension/firefox.svg" style="height: 35px; box-shadow: 0px 0px 0px white">
+				Get it for Firefox</p>
+			</div>
+		</a>
+	</div>	
+</div>
+
+If you're operating from **another browser** and/or want to do it manually, [here is how to do it](https://intercom.help/phantombuster/help-home/how-to-get-your-cookies-without-using-our-browser-extension).
+
+## 5. Add a Google Spreadsheet 📑
+Below your session cookies you’ll find Spreadsheet URL.
+
+Add in the Spreadsheet URL textbox the link of a Google spreadsheet with this same format **(Share option must be OPEN)**.
+
+Your spreadsheet should contain a list of LinkedIn Sales Navigator Profile URLs (**one link per row**).
+
+You can specify the name of the column that contains the profile links. Simply enter the column name in the next text field.
+You can also enter a single SN Profile URL directly in the field.
+
+
+# Click on Launch & Enjoy!
+It’s done! All that is left to do is to click on "launch" and watch your API do the work for you!
+
+<center>![](https://phantombuster.imgix.net/api-store/launch.JPG)</center>
+
+# Limits
+
+Please be aware that this API, like most of our LinkedIn APIs, will manipulate your own account on your behalf.
+
+The API needs to visit the profile to convert the URL and we've noticed that visiting more than 80 profiles per day will almost always result in LinkedIn **invalidating your session cookie** (that is, logging you out). In that regard we recommend spacing your launches.
