ff9ff0e6f137dbf4e13b7c31c14cf2f7c0f97462
guillaumeboiret <guillaume.boiret@gmail.com>
2019-05-10 11:23:21 +0200

    Convert mdpp - Remove gitignore option

--- convert-all-mdpp.sh
+++ convert-all-mdpp.sh
@@ -16,34 +16,35 @@ for file in ./store/DEV/*; do
 	if $(markdown-pp "$file/$input.mdpp" -o "$file/$input.md" 2> /dev/null)
 	then
 		echo $okMsg "Transpiling..."
-		#-- Add MD name into the local .gitignore
-		cd "$file";
-		gitignore=".gitignore"
-		if [ ! -f $gitignore ] || [ ! -s $gitignore ]; then
-			echo "$input.md" > $gitignore
-			echo $okMsg "MD gitignore created..."
-		else
-			flag=false
-			while IFS="" read -r p || [ -n "$p" ]
-			do
-				line=$(printf '%s\n' "$p")
-				if [[ "$line" == "$input.md" ]]
-				then
-					flag=true
-				fi
-			done < $gitignore
 
-			if [[ "$flag" == false ]]
-			then
-				if [[ ! $(tail -c1 $gitignore | wc -l) -gt 0 ]]
-				then
-					echo "" >> $gitignore
-				fi
-				echo "$input.md" >> $gitignore
-				echo $okMsg "MD gitignored updated..."
-			fi
-		fi
-		cd ~-
+		#-- Add MD name into the local .gitignore
+		# cd "$file";
+		# gitignore=".gitignore"
+		# if [ ! -f $gitignore ] || [ ! -s $gitignore ]; then
+		# 	echo "$input.md" > $gitignore
+		# 	echo $okMsg "MD gitignore created..."
+		# else
+		# 	flag=false
+		# 	while IFS="" read -r p || [ -n "$p" ]
+		# 	do
+		# 		line=$(printf '%s\n' "$p")
+		# 		if [[ "$line" == "$input.md" ]]
+		# 		then
+		# 			flag=true
+		# 		fi
+		# 	done < $gitignore
+
+		# 	if [[ "$flag" == false ]]
+		# 	then
+		# 		if [[ ! $(tail -c1 $gitignore | wc -l) -gt 0 ]]
+		# 		then
+		# 			echo "" >> $gitignore
+		# 		fi
+		# 		echo "$input.md" >> $gitignore
+		# 		echo $okMsg "MD gitignored updated..."
+		# 	fi
+		# fi
+		# cd ~-
 
 		#-- Move file in the release folder
 		if mv "./store/DEV/$input/$input.md" "./store/$output/$output.md" 2> /dev/null
